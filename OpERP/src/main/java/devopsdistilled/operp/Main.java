d3058ec64ec027418eb72b831e5ebba52f127709
invincibleelf <invincibleelf@gmail.com>
2013-02-17 08:23:29 +0545

    Added DevOpsDistilled in Main

--- OpERP/src/main/java/devopsdistilled/operp/Main.java
+++ OpERP/src/main/java/devopsdistilled/operp/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
 	System.out.println("OpERP");
 	System.out.println("Enterprise Resource Planning for Distribution Companies");
+	System.out.println("DevOpsDistilled");
     }
 
 }
