7bdcb7e0283c7e0090eb8005f631e8acbe63dc4e
invincibleelf <invincibleelf@gmail.com>
2013-02-17 08:55:18 +0545

    Removed Obsolete .xml config files

--- OpERP/src/main/java/devopsdistilled/operp/server/ServerApp.java
+++ OpERP/src/main/java/devopsdistilled/operp/server/ServerApp.java
@@ -3,11 +3,12 @@ package devopsdistilled.operp.server;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import devopsdistilled.operp.server.config.AppConfig;
+
 public class ServerApp {
     public static void main(String[] args) {
 
-	ApplicationContext ctx = new AnnotationConfigApplicationContext(
-		"devopsdistilled.operp.server");
+	ApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
 
 	System.out.println(ctx.toString());
     }
