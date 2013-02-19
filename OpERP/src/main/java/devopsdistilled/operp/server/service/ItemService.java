17361c26b4e6610723673f18594c75be1273fea2
Kapil Koju <kplx2.0@gmail.com>
2013-02-19 15:06:09 +0545

    Removed all Generic Service dependencies

--- OpERP/src/main/java/devopsdistilled/operp/server/service/ItemService.java
+++ OpERP/src/main/java/devopsdistilled/operp/server/service/ItemService.java
@@ -1,7 +1,5 @@
 package devopsdistilled.operp.server.service;
 
-import devopsdistilled.operp.server.model.Item;
 
-public interface ItemService extends GenericService<Item, Long> {
-    public void create(String name);
+public interface ItemService {
 }
--- OpERP/src/main/java/devopsdistilled/operp/server/service/impl/ItemServiceImpl.java
+++ OpERP/src/main/java/devopsdistilled/operp/server/service/impl/ItemServiceImpl.java
@@ -2,32 +2,9 @@ package devopsdistilled.operp.server.service.impl;
 
 import org.springframework.stereotype.Service;
 
-import devopsdistilled.operp.server.dao.GenericDao;
-import devopsdistilled.operp.server.dao.ItemDao;
-import devopsdistilled.operp.server.model.Item;
 import devopsdistilled.operp.server.service.ItemService;
 
 @Service
-public class ItemServiceImpl extends GenericServiceImpl<Item, Long> implements
-	ItemService {
-
-    private ItemDao dao;
-
-    public void setDao(ItemDao dao) {
-	this.dao = dao;
-    }
-
-    @Override
-    protected GenericDao<Item, Long> getDao() {
-	return dao;
-    }
-
-    @Override
-    public void create(String name) {
-	Item item = new Item();
-	item.setName(name);
-	getDao().create(item);
-    }
-
+public class ItemServiceImpl implements ItemService {
 
 }
