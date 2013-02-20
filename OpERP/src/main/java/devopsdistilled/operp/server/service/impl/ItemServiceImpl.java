25f8c14b8f2262d331303c809cd66cdef49d31bb
Kapil Koju <kplx2.0@gmail.com>
2013-02-20 19:50:45 +0545

    add @Transactional
    fixes persisting to database of Item

--- OpERP/src/main/java/devopsdistilled/operp/server/service/impl/ItemServiceImpl.java
+++ OpERP/src/main/java/devopsdistilled/operp/server/service/impl/ItemServiceImpl.java
@@ -4,11 +4,13 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import devopsdistilled.operp.server.model.Item;
 import devopsdistilled.operp.server.service.ItemService;
 
 @Repository("ItemService")
+@Transactional
 public class ItemServiceImpl implements ItemService {
 
     @PersistenceContext
