aa6dbc2be22238c587affa9c4d49ae7b388f4a4c
Kapil Koju <kplx2.0@gmail.com>
2013-02-21 23:04:52 +0545

    Configure RMI service Exporter

--- OpERP/src/main/java/devopsdistilled/operp/server/config/AppConfig.java
+++ OpERP/src/main/java/devopsdistilled/operp/server/config/AppConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import(JpaConfig.class)
+@Import({ JpaConfig.class, RmiConfig.class })
 public class AppConfig {
 
 }
