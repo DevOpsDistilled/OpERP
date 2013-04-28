173b923480876fa17e474d180c9768674d474145
Kapil Koju <kplx2.0@gmail.com>
2013-04-28 07:42:05 +0545

    Configure with @ComponentScan
    Remove packages in AnnotationConfigApplicationContext and add to
    AppContext with @ComponentScan annotation

--- OpERP/src/main/java/devopsdistilled/operp/server/ServerApp.java
+++ OpERP/src/main/java/devopsdistilled/operp/server/ServerApp.java
@@ -2,6 +2,7 @@ package devopsdistilled.operp.server;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import devopsdistilled.operp.client.context.AppContext;
 import devopsdistilled.operp.server.model.Item;
 import devopsdistilled.operp.server.service.ItemService;
 
@@ -9,8 +10,7 @@ public class ServerApp {
 	public static void main(String[] args) {
 
 		@SuppressWarnings("resource")
-		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(
-				"devopsdistilled.operp.server");
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppContext.class);
 		ItemService itemService = ctx.getBean(ItemService.class);
 		Item item = itemService.createItem("Test Item from Server");
 		System.out.println(item);
