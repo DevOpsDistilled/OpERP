3dc24d009bf26fecff602cd036af41f896bfa92d
Kapil Koju <kplx2.0@gmail.com>
2013-04-30 21:34:59 +0545

    Add ApplicationContext config with embedded database

---
+++ OpERP/src/test/java/devopsdistilled/operp/server/context/JpaH2TestContext.java
@@ -0,0 +1,99 @@
+package devopsdistilled.operp.server.context;
+
+import java.util.Properties;
+
+import javax.inject.Inject;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.jpa.JpaDialect;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaDialect;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableJpaRepositories("devopsdistilled.operp.server.data")
+@EnableTransactionManagement
+@PropertySource("server/jdbc.properties")
+@ComponentScan("devopsdistilled.operp.server.data")
+public class JpaH2TestContext {
+
+	@Inject
+	private Environment env;
+
+	@Value("devopsdistilled.operp.server.data.entity")
+	private String packagesToScan;
+
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer getPropertySourcesPlaceholderConfigurer() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
+
+	@Bean
+	public DataSource dataSource() {
+		return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2)
+				.setName("OpERP").build();
+	}
+
+	@Bean
+	public JpaVendorAdapter jpaVendorAdapter() {
+		HibernateJpaVendorAdapter jpaVendorAdapter = new HibernateJpaVendorAdapter();
+		jpaVendorAdapter.setDatabase(Database.MYSQL);
+		jpaVendorAdapter.setGenerateDdl(true);
+		jpaVendorAdapter.setShowSql(true);
+		jpaVendorAdapter
+				.setDatabasePlatform("org.hibernate.dialect.MySQL5Dialect");
+		return jpaVendorAdapter;
+	}
+
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+		LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
+		emf.setDataSource(this.dataSource());
+		emf.setJpaVendorAdapter(this.jpaVendorAdapter());
+		emf.setPackagesToScan(packagesToScan);
+		emf.setJpaProperties(this.hibernateProperties());
+		return emf;
+	}
+
+	@Bean
+	public JpaDialect jpaDialect() {
+		return new HibernateJpaDialect();
+	}
+
+	@Bean
+	public JpaTransactionManager transactionManager() {
+		JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(entityManagerFactory()
+				.getObject());
+		transactionManager.setJpaDialect(jpaDialect());
+		return transactionManager;
+	}
+
+	@Bean
+	public Properties hibernateProperties() {
+		Properties hibernateProps = new Properties();
+		hibernateProps.setProperty("hibernate.hbm2ddl.auto", "create");
+		return hibernateProps;
+	}
+
+	@Bean
+	public PersistenceExceptionTranslationPostProcessor exceptionTranslationPostProcessor() {
+		return new PersistenceExceptionTranslationPostProcessor();
+	}
+
+}
