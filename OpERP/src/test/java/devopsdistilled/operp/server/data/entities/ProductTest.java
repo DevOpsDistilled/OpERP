1fb8d9a611993d4796ab56a6912d32fcda2f8751
Kapil Koju <kplx2.0@gmail.com>
2013-05-01 12:13:58 +0545

    Have dummy ProductTest methods

--- OpERP/src/test/java/devopsdistilled/operp/server/data/entities/ProductTest.java
+++ OpERP/src/test/java/devopsdistilled/operp/server/data/entities/ProductTest.java
@@ -9,11 +9,11 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-import devopsdistilled.operp.server.context.JpaH2TestContext;
+import devopsdistilled.operp.server.context.AppTestContext;
 import devopsdistilled.operp.server.data.repo.ProductRepository;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = JpaH2TestContext.class)
+@ContextConfiguration(classes = AppTestContext.class)
 @Transactional
 public class ProductTest {
 
@@ -23,7 +23,5 @@ public class ProductTest {
 	@Test
 	public void test() {
 		Assert.assertNotNull(productRepository);
-		
 	}
-
 }
