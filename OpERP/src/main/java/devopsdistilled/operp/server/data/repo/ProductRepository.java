ba3b05b1e19c4e6549a1baadf9ac8113bfbfd586
Kapil Koju <kplx2.0@gmail.com>
2013-05-02 12:16:56 +0545

    Add findByProductName()

--- OpERP/src/main/java/devopsdistilled/operp/server/data/repo/ProductRepository.java
+++ OpERP/src/main/java/devopsdistilled/operp/server/data/repo/ProductRepository.java
@@ -1,5 +1,7 @@
 package devopsdistilled.operp.server.data.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import devopsdistilled.operp.server.data.entity.Product;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-	
+
+	public List<Product> findByProductName(String productName);
+
 }
