fef33a8b82dcf1fbf420f7298711e4a488665752
Kapil Koju <kplx2.0@gmail.com>
2013-05-06 19:59:15 +0545

    Add ProductRepositoryCustom

---
+++ OpERP/src/main/java/devopsdistilled/operp/server/data/repo/custom/ProductRepositoryCustom.java
@@ -0,0 +1,7 @@
+package devopsdistilled.operp.server.data.repo.custom;
+
+import devopsdistilled.operp.server.data.entity.Product;
+
+public interface ProductRepositoryCustom {
+	public Product saveCustom(Product product);
+}
