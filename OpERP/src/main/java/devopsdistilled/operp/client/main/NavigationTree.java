ebe797812b582bcba9a8bbe563b71702f24c8268
Kapil Koju <kplx2.0@gmail.com>
2013-07-09 06:33:48 +0545

    Remove obsolete classes

--- OpERP/src/main/java/devopsdistilled/operp/client/main/NavigationTree.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/main/NavigationTree.java
@@ -3,7 +3,6 @@ package devopsdistilled.operp.client.main;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.swing.JFrame;
 import javax.swing.JTree;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
@@ -12,7 +11,7 @@ public class NavigationTree {
 
 	Map<String, DefaultMutableTreeNode> nodeMap;
 
-	private DefaultTreeModel treeModel;
+	private final DefaultTreeModel treeModel;
 	private JTree tree;
 
 	public JTree getTree() {
@@ -67,27 +66,5 @@ public class NavigationTree {
 			nodeMap.put(taskPane.toString(), node);
 		} else
 			throw new IllegalArgumentException("Illegal arguements in addNode");
-	}
-
-	public static void main(String[] args) {
-		NavigationTree nav = new NavigationTree();
-		JFrame frame = new JFrame();
-		frame.getContentPane().add(nav.getTree());
-
-		nav.addNode(new TestTaskPane("First one"),
-				new DefaultTaskPane().toString(), 0);
-
-		nav.addNode(new TestTaskPane("Zero"),
-				new TestTaskPane("First one").toString(), 0);
-
-		nav.addNode(new TestTaskPane("One"),
-				new TestTaskPane("First one").toString(), 1);
-		nav.addNode(new TestTaskPane("Two"), 1);
-		nav.addNode(new TestTaskPane("Three"));
-
-		nav.treeModel.reload();
-		frame.setVisible(true);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setBounds(20, 20, 200, 200);
 	}
 }
