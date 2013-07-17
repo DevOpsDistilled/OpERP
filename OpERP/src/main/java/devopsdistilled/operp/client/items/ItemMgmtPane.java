ff74f750f5a8258c9cfa4abfab7d26e5584eea49
Kapil Koju <kplx2.0@gmail.com>
2013-07-17 13:40:16 +0545

    Create sample Pane to create new Item.
    OPEN - task 20: Create a working item module workflow 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/20

--- OpERP/src/main/java/devopsdistilled/operp/client/items/ItemMgmtPane.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/items/ItemMgmtPane.java
@@ -1,23 +1,45 @@
 package devopsdistilled.operp.client.items;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import net.miginfocom.swing.MigLayout;
 import devopsdistilled.operp.client.abstracts.TaskPane;
 
 public final class ItemMgmtPane extends TaskPane {
+
+	private JComponent owner;
 
 	@Override
 	public String toString() {
 		return new String("Items");
 	}
 
+	/**
+	 * @wbp.parser.entryPoint
+	 */
 	@Override
 	public JComponent getPane() {
 		JPanel pane = new JPanel();
-		pane.add(new JLabel("Items Management"));
+		pane.setLayout(new MigLayout("", "[]", "[][][]"));
+
+		JLabel lblitemManagement = new JLabel("Item Management");
+		pane.add(lblitemManagement, "cell 0 0");
+
+		JButton btnNewItemButton = new JButton("New Item Button");
+		btnNewItemButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				new NewItemPane().getDialog();
+			}
+		});
+		pane.add(btnNewItemButton, "cell 0 1");
+
 		return pane;
 	}
-
 }
---
+++ OpERP/src/main/java/devopsdistilled/operp/client/items/NewItemPane.java
@@ -0,0 +1,71 @@
+package devopsdistilled.operp.client.items;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JComponent;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import net.miginfocom.swing.MigLayout;
+
+public class NewItemPane {
+
+	private final JPanel pane;
+	private JComponent owner;
+	private final JTextField textField;
+	private final JTextField textField_3;
+
+	public NewItemPane() {
+		pane = new JPanel();
+		pane.setLayout(new MigLayout("", "[][][grow]", "[][][][][]"));
+
+		JLabel lblItemId = new JLabel("Item ID");
+		pane.add(lblItemId, "cell 0 0,alignx trailing");
+
+		textField = new JTextField();
+		pane.add(textField, "cell 2 0,growx");
+		textField.setColumns(10);
+
+		JLabel lblProductName = new JLabel("Product Name");
+		pane.add(lblProductName, "cell 0 1,alignx trailing");
+
+		JComboBox comboBox = new JComboBox();
+		pane.add(comboBox, "cell 2 1,growx");
+
+		JLabel lblBrandName = new JLabel("Brand Name");
+		pane.add(lblBrandName, "cell 0 2,alignx trailing");
+
+		JComboBox comboBox_1 = new JComboBox();
+		pane.add(comboBox_1, "cell 2 2,growx");
+
+		JLabel lblPrice = new JLabel("Price");
+		pane.add(lblPrice, "cell 0 3,alignx trailing");
+
+		textField_3 = new JTextField();
+		pane.add(textField_3, "cell 2 3,growx");
+		textField_3.setColumns(10);
+
+		JButton btnCancel = new JButton("Cancel");
+		pane.add(btnCancel, "flowx,cell 2 4");
+
+		JButton btnReset = new JButton("Reset");
+		pane.add(btnReset, "cell 2 4");
+
+		JButton btnSave = new JButton("Save");
+		pane.add(btnSave, "cell 2 4");
+	}
+
+	public JComponent getPane() {
+		return pane;
+	}
+
+	public JDialog getDialog() {
+		JDialog dialog = new JDialog();
+		dialog.add(getPane());
+		dialog.setSize(640, 800);
+		dialog.setVisible(true);
+		return dialog;
+	}
+}
