341f43e7dc8b72f95c30ccac4f12201573210cef
Kapil Koju <kplx2.0@gmail.com>
2013-07-18 06:10:35 +0545

    Add getRepo() to get subclass repo
    OPEN - task 20: Create a working item module workflow 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/20

--- OpERP/src/main/java/devopsdistilled/operp/server/data/service/impl/AbstractServiceImpl.java
+++ OpERP/src/main/java/devopsdistilled/operp/server/data/service/impl/AbstractServiceImpl.java
@@ -10,95 +10,95 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import devopsdistilled.operp.server.data.service.AbstractService;
 
-public class AbstractServiceImpl<T, ID extends Serializable> implements
-		AbstractService<T, ID> {
+public abstract class AbstractServiceImpl<T, ID extends Serializable>
+		implements AbstractService<T, ID> {
 
 	private static final long serialVersionUID = 4892118695516828793L;
 
-	protected JpaRepository<T, ID> repo;
+	protected abstract JpaRepository<T, ID> getRepo();
 
 	@Override
 	public List<T> findAll() {
-		return repo.findAll();
+		return getRepo().findAll();
 	}
 
 	@Override
 	public List<T> findAll(Sort sort) {
-		return repo.findAll(sort);
+		return getRepo().findAll(sort);
 	}
 
 	@Override
 	public <S extends T> List<S> save(Iterable<S> entities) {
-		return repo.save(entities);
+		return getRepo().save(entities);
 	}
 
 	@Override
 	public void flush() {
-		repo.flush();
+		getRepo().flush();
 	}
 
 	@Override
 	public T saveAndFlush(T entity) {
-		return repo.saveAndFlush(entity);
+		return getRepo().saveAndFlush(entity);
 	}
 
 	@Override
 	public void deleteInBatch(Iterable<T> entities) {
-		repo.deleteInBatch(entities);
+		getRepo().deleteInBatch(entities);
 	}
 
 	@Override
 	public void deleteAllInBatch() {
-		repo.deleteAllInBatch();
+		getRepo().deleteAllInBatch();
 	}
 
 	@Override
 	public Page<T> findAll(Pageable pageable) {
-		return findAll(pageable);
+		return getRepo().findAll(pageable);
 	}
 
 	@Override
 	public <S extends T> S save(S entity) {
-		return repo.save(entity);
+		return getRepo().save(entity);
 	}
 
 	@Override
 	public T findOne(ID id) {
-		return repo.findOne(id);
+		return getRepo().findOne(id);
 	}
 
 	@Override
 	public boolean exists(ID id) {
-		return repo.exists(id);
+		return getRepo().exists(id);
 	}
 
 	@Override
 	public long count() {
-		return repo.count();
+		return getRepo().count();
 	}
 
 	@Override
 	public void delete(ID id) {
-		repo.delete(id);
+		getRepo().delete(id);
 	}
 
 	@Override
 	public void delete(T entity) {
-		repo.delete(entity);
+		getRepo().delete(entity);
 	}
 
 	@Override
 	public void delete(Iterable<? extends T> entities) {
-		repo.delete(entities);
+		getRepo().delete(entities);
 	}
 
 	@Override
 	public void deleteAll() {
-		repo.deleteAll();
+		getRepo().deleteAll();
 	}
 
 	@Override
 	public Iterable<T> findAll(Iterable<ID> ids) {
-		return repo.findAll(ids);
+		return getRepo().findAll(ids);
 	}
 }
