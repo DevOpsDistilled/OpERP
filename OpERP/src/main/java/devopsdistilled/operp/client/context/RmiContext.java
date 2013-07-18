6d019dd6a4f8c9601577395c2e77c78acfb5ccc2
Kapil Koju <kplx2.0@gmail.com>
2013-07-18 06:28:26 +0545

    Configure RmiProxy Bean for Product and Brand
    OPEN - task 20: Create a working item module workflow 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/20

--- OpERP/src/main/java/devopsdistilled/operp/client/context/RmiContext.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/context/RmiContext.java
@@ -4,16 +4,34 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.remoting.rmi.RmiProxyFactoryBean;
 
+import devopsdistilled.operp.server.data.service.items.BrandService;
 import devopsdistilled.operp.server.data.service.items.ItemService;
+import devopsdistilled.operp.server.data.service.items.ProductService;
 
 @Configuration
 public class RmiContext {
-	
+
 	@Bean
 	public RmiProxyFactoryBean itemService() {
 		RmiProxyFactoryBean rmiProxy = new RmiProxyFactoryBean();
 		rmiProxy.setServiceUrl("rmi://127.0.1.1:1099/ItemService");
 		rmiProxy.setServiceInterface(ItemService.class);
+		return rmiProxy;
+	}
+
+	@Bean
+	public RmiProxyFactoryBean productService() {
+		RmiProxyFactoryBean rmiProxy = new RmiProxyFactoryBean();
+		rmiProxy.setServiceUrl("rmi://127.0.1.1:1099/ProductService");
+		rmiProxy.setServiceInterface(ProductService.class);
+		return rmiProxy;
+	}
+
+	@Bean
+	public RmiProxyFactoryBean brandService() {
+		RmiProxyFactoryBean rmiProxy = new RmiProxyFactoryBean();
+		rmiProxy.setServiceUrl("rmi://127.0.1.1:1099/BrandService");
+		rmiProxy.setServiceInterface(BrandService.class);
 		return rmiProxy;
 	}
 }
