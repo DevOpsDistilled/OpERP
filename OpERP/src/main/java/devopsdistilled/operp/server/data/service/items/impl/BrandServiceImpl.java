362428560f524aadc60b5541c8835e7508b7e228
Kapil Koju <kplx2.0@gmail.com>
2013-07-19 11:13:30 +0545

    Add @Service Annotation
    @Service annotation auto export these classes as @Bean

--- OpERP/src/main/java/devopsdistilled/operp/server/data/service/items/impl/BrandServiceImpl.java
+++ OpERP/src/main/java/devopsdistilled/operp/server/data/service/items/impl/BrandServiceImpl.java
@@ -3,12 +3,14 @@ package devopsdistilled.operp.server.data.service.items.impl;
 import javax.inject.Inject;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
 
 import devopsdistilled.operp.server.data.entity.items.Brand;
 import devopsdistilled.operp.server.data.repo.items.BrandRepository;
 import devopsdistilled.operp.server.data.service.impl.AbstractServiceImpl;
 import devopsdistilled.operp.server.data.service.items.BrandService;
 
+@Service
 public class BrandServiceImpl extends AbstractServiceImpl<Brand, Long>
 		implements BrandService {
 
--- OpERP/src/main/java/devopsdistilled/operp/server/data/service/items/impl/ProductServiceImpl.java
+++ OpERP/src/main/java/devopsdistilled/operp/server/data/service/items/impl/ProductServiceImpl.java
@@ -3,12 +3,14 @@ package devopsdistilled.operp.server.data.service.items.impl;
 import javax.inject.Inject;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
 
 import devopsdistilled.operp.server.data.entity.items.Product;
 import devopsdistilled.operp.server.data.repo.items.ProductRepository;
 import devopsdistilled.operp.server.data.service.impl.AbstractServiceImpl;
 import devopsdistilled.operp.server.data.service.items.ProductService;
 
+@Service
 public class ProductServiceImpl extends AbstractServiceImpl<Product, Long>
 		implements ProductService {
 
