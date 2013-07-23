309d596e0fbedd35a512d13e30938b8cf0081d78
Kapil Koju <kplx2.0@gmail.com>
2013-07-23 16:24:27 +0545

    Implement ItemController to create new Item

--- OpERP/src/main/java/devopsdistilled/operp/client/items/ItemController.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/items/ItemController.java
@@ -1,5 +1,11 @@
 package devopsdistilled.operp.client.items;
 
+import devopsdistilled.operp.server.data.entity.items.Item;
+
 public interface ItemController {
+
+	public void createItem();
+
+	public void editItem(Item item);
 
 }
--- OpERP/src/main/java/devopsdistilled/operp/client/items/ItemControllerImpl.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/items/ItemControllerImpl.java
@@ -1,5 +1,22 @@
 package devopsdistilled.operp.client.items;
 
-public class ItemControllerImpl {
+import devopsdistilled.operp.client.ClientApp;
+import devopsdistilled.operp.server.data.entity.items.Item;
+
+public class ItemControllerImpl implements ItemController {
+
+	@Override
+	public void createItem() {
+		ItemPane itemPane = ClientApp.getApplicationContext().getBean(
+				ItemPane.class);
+		itemPane.init();
+		//itemPane.setOwner(getPane());
+		itemPane.getDialog();
+	}
+
+	@Override
+	public void editItem(Item item) {
+		
+	}
 
 }
