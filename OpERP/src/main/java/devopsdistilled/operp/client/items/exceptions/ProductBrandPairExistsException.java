ae4180160b98557e70dc1742a66de2c5db31ec4c
Kapil Koju <kplx2.0@gmail.com>
2013-07-25 20:33:32 +0545

    Create exceptions to give controller.validate() signals
    OPEN - task 32: Add "Create Item" functionalities 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/32

---
+++ OpERP/src/main/java/devopsdistilled/operp/client/items/exceptions/ProductBrandPairExistsException.java
@@ -0,0 +1,7 @@
+package devopsdistilled.operp.client.items.exceptions;
+
+public class ProductBrandPairExistsException extends Exception {
+
+	private static final long serialVersionUID = -6474661245907152963L;
+
+}
