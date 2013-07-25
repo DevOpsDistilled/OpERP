315467bbc7ca0a8f2351aa874068103d688a2ad0
Kapil Koju <kplx2.0@gmail.com>
2013-07-25 08:36:14 +0545

    Declare Observer for BrandModel
    OPEN - task 32: Add "Create Item" functionalities 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/32

---
+++ OpERP/src/main/java/devopsdistilled/operp/client/items/models/observers/BrandModelObserver.java
@@ -0,0 +1,8 @@
+package devopsdistilled.operp.client.items.models.observers;
+
+import devopsdistilled.operp.client.abstracts.EntityObserver;
+import devopsdistilled.operp.server.data.entity.items.Brand;
+
+public interface BrandModelObserver extends EntityObserver<Brand> {
+
+}
