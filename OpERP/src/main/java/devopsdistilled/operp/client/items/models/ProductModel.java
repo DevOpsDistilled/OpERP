ecde705c7361e3ac7ea3f73e1ae91456a4bb13bc
Kapil Koju <kplx2.0@gmail.com>
2013-07-25 08:34:44 +0545

    Define ApplicationModel i.e ProductModel for Product Entity
    OPEN - task 32: Add "Create Item" functionalities 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/32

---
+++ OpERP/src/main/java/devopsdistilled/operp/client/items/models/ProductModel.java
@@ -0,0 +1,10 @@
+package devopsdistilled.operp.client.items.models;
+
+import devopsdistilled.operp.client.abstracts.EntityModel;
+import devopsdistilled.operp.client.items.models.observers.ProductModelObserver;
+import devopsdistilled.operp.server.data.entity.items.Product;
+import devopsdistilled.operp.server.data.service.items.ProductService;
+
+public interface ProductModel extends EntityModel<Product, ProductService, ProductModelObserver, Long> {
+
+}
