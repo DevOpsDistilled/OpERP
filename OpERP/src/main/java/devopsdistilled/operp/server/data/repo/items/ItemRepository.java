9758fa614003f87a68eca1b9944289fe7a4f0407
Kapil Koju <kplx2.0@gmail.com>
2013-07-25 23:46:57 +0545

    Correct return type of the methods
    OPEN - task 32: Add "Create Item" functionalities 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/32

--- OpERP/src/main/java/devopsdistilled/operp/server/data/repo/items/ItemRepository.java
+++ OpERP/src/main/java/devopsdistilled/operp/server/data/repo/items/ItemRepository.java
@@ -1,11 +1,18 @@
 package devopsdistilled.operp.server.data.repo.items;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import devopsdistilled.operp.server.data.entity.items.Item;
-	
+
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
+
+	List<Item> findByProductNameAndBrandName(String productName,
+			String brandName);
+
+	List<Item> findByItemName(String itemName);
 
 }
