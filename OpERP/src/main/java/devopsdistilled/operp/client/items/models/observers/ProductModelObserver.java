ad92afdadb56850c39ebb516a601a8477f66335e
Kapil Koju <kplx2.0@gmail.com>
2013-07-25 08:35:05 +0545

    Declare Observer for ProductModel
    OPEN - task 32: Add "Create Item" functionalities 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/32

---
+++ OpERP/src/main/java/devopsdistilled/operp/client/items/models/observers/ProductModelObserver.java
@@ -0,0 +1,8 @@
+package devopsdistilled.operp.client.items.models.observers;
+
+import devopsdistilled.operp.client.abstracts.EntityObserver;
+import devopsdistilled.operp.server.data.entity.items.Product;
+
+public interface ProductModelObserver extends EntityObserver<Product> {
+
+}
