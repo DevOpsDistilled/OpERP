76bf0cdd226713e7e5a1a7dcb1ab8bceab3c25a1
Kapil Koju <kplx2.0@gmail.com>
2013-07-25 20:42:27 +0545

    Define validate() and save()
    OPEN - task 32: Add "Create Item" functionalities 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/32

--- OpERP/src/main/java/devopsdistilled/operp/client/items/controllers/impl/CreateItemPaneControllerImpl.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/items/controllers/impl/CreateItemPaneControllerImpl.java
@@ -3,6 +3,8 @@ package devopsdistilled.operp.client.items.controllers.impl;
 import javax.inject.Inject;
 
 import devopsdistilled.operp.client.items.controllers.CreateItemPaneController;
+import devopsdistilled.operp.client.items.exceptions.ItemNameExistsException;
+import devopsdistilled.operp.client.items.exceptions.ProductBrandPairExistsException;
 import devopsdistilled.operp.client.items.models.BrandModel;
 import devopsdistilled.operp.client.items.models.CreateItemPaneModel;
 import devopsdistilled.operp.client.items.models.ItemModel;
@@ -36,9 +38,24 @@ public class CreateItemPaneControllerImpl implements CreateItemPaneController {
 	}
 
 	@Override
-	public void validate(Item item) {
-		// TODO Auto-generated method stub
+	public void validate(Item item) throws ProductBrandPairExistsException,
+			ItemNameExistsException {
 
+		if (itemModel.getService().isProductBrandPairExists(
+				item.getProduct().getProductName(),
+				item.getBrand().getBrandName())) {
+
+			throw new ProductBrandPairExistsException();
+		}
+
+		if (itemModel.getService().isItemNameExists(item.getItemName())) {
+
+			throw new ItemNameExistsException();
+		}
 	}
 
+	@Override
+	public Item save(Item item) {
+		return itemModel.getService().saveAndFlush(item);
+	}
 }
