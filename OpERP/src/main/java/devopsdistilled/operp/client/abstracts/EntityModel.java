c55772bcd970503a44e429ccf6b1249392959c96
Kapil Koju <kplx2.0@gmail.com>
2013-07-25 14:06:43 +0545

    Use non-generic EntityModel
    OPEN - task 33: Problem with EntityModelObserver architecture 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/33

--- OpERP/src/main/java/devopsdistilled/operp/client/abstracts/EntityModel.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/abstracts/EntityModel.java
@@ -6,7 +6,7 @@ import java.util.List;
 import devopsdistilled.operp.server.data.entity.Entiti;
 import devopsdistilled.operp.server.data.service.EntityService;
 
-public interface EntityModel<E extends Entiti, ES extends EntityService<E, ID>, EO extends EntityObserver<E>, ID extends Serializable>
+public interface EntityModel<E extends Entiti, ES extends EntityService<E, ID>, EO extends EntityObserver, ID extends Serializable>
 		extends Model<EO> {
 
 	public List<E> getEntities();
