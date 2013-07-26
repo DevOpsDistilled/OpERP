eb51777ef17bffdbc39093e41938263f860fc156
Kapil Koju <kplx2.0@gmail.com>
2013-07-26 23:25:10 +0545

    @Inject ItemModel and registerObserver
    OPEN - task 41: Design "ListItemPane" as per new MVC model 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/41

--- OpERP/src/main/java/devopsdistilled/operp/client/items/controllers/impl/ListItemPaneControllerImpl.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/items/controllers/impl/ListItemPaneControllerImpl.java
@@ -3,6 +3,7 @@ package devopsdistilled.operp.client.items.controllers.impl;
 import javax.inject.Inject;
 
 import devopsdistilled.operp.client.items.controllers.ListItemPaneController;
+import devopsdistilled.operp.client.items.models.ItemModel;
 import devopsdistilled.operp.client.items.models.ListItemPaneModel;
 import devopsdistilled.operp.client.items.views.ListItemPane;
 
@@ -14,10 +15,14 @@ public class ListItemPaneControllerImpl implements ListItemPaneController {
 	@Inject
 	private ListItemPaneModel model;
 
+	@Inject
+	private ItemModel itemModel;
+
 	@Override
 	public void init() {
 		view.init();
 		model.registerObserver(view);
+		itemModel.registerObserver(view);
 	}
 
 }
