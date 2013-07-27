333ab3d64a8ea8f5b9bd45764bf471d526aa3a06
Kapil Koju <kplx2.0@gmail.com>
2013-07-27 15:28:41 +0545

    Add throws clause to validate()
    OPEN - task 48: Create "Edit Item" functionality 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/48

--- OpERP/src/main/java/devopsdistilled/operp/client/items/controllers/EditItemPaneController.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/items/controllers/EditItemPaneController.java
@@ -1,6 +1,9 @@
 package devopsdistilled.operp.client.items.controllers;
 
 import devopsdistilled.operp.client.abstracts.SubTaskPaneController;
+import devopsdistilled.operp.client.items.exceptions.ItemNameExistsException;
+import devopsdistilled.operp.client.items.exceptions.NullFieldException;
+import devopsdistilled.operp.client.items.exceptions.ProductBrandPairExistsException;
 import devopsdistilled.operp.server.data.entity.items.Item;
 
 public interface EditItemPaneController extends SubTaskPaneController {
@@ -9,5 +12,6 @@ public interface EditItemPaneController extends SubTaskPaneController {
 
 	public Item save(Item item);
 
-	public void validate(Item item);
+	public void validate(Item item) throws NullFieldException,
+			ProductBrandPairExistsException, ItemNameExistsException;
 }
