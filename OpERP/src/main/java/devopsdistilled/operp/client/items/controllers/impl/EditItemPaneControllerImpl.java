b0cf09446d0284e5df93ccfe404940e86c23e70c
Kapil Koju <kplx2.0@gmail.com>
2013-07-27 14:47:54 +0545

    @Inject ItemModel, ProductModel and BrandModel
    registerObserver() for productModel and brandModel
    
    OPEN - task 48: Create "Edit Item" functionality 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/48

--- OpERP/src/main/java/devopsdistilled/operp/client/items/controllers/impl/EditItemPaneControllerImpl.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/items/controllers/impl/EditItemPaneControllerImpl.java
@@ -3,7 +3,10 @@ package devopsdistilled.operp.client.items.controllers.impl;
 import javax.inject.Inject;
 
 import devopsdistilled.operp.client.items.controllers.EditItemPaneController;
+import devopsdistilled.operp.client.items.models.BrandModel;
 import devopsdistilled.operp.client.items.models.EditItemPaneModel;
+import devopsdistilled.operp.client.items.models.ItemModel;
+import devopsdistilled.operp.client.items.models.ProductModel;
 import devopsdistilled.operp.client.items.views.EditItemPane;
 import devopsdistilled.operp.server.data.entity.items.Item;
 
@@ -15,6 +18,15 @@ public class EditItemPaneControllerImpl implements EditItemPaneController {
 	@Inject
 	private EditItemPaneModel model;
 
+	@Inject
+	private ItemModel itemModel;
+
+	@Inject
+	private ProductModel productModel;
+
+	@Inject
+	private BrandModel brandModel;
+
 	@Override
 	public void init() {
 
@@ -25,6 +37,8 @@ public class EditItemPaneControllerImpl implements EditItemPaneController {
 		view.init();
 		model.setItem(item);
 		model.registerObserver(view);
+		productModel.registerObserver(view);
+		brandModel.registerObserver(view);
 	}
 
 }
