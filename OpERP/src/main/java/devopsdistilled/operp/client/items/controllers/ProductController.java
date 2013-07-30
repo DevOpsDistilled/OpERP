35484cbe7f8af984e1726c431bfe4454d0330627
Kapil Koju <kplx2.0@gmail.com>
2013-07-30 23:26:27 +0545

    Create ProductController extending EntityController
    OPEN - task 51: Create CRUD packages for Product entity 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/51

--- OpERP/src/main/java/devopsdistilled/operp/client/items/controllers/ProductController.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/items/controllers/ProductController.java
@@ -1,7 +1,8 @@
 package devopsdistilled.operp.client.items.controllers;
 
+import devopsdistilled.operp.client.abstracts.EntityController;
 import devopsdistilled.operp.server.data.entity.items.Product;
 
-public interface ProductController {
-	public Product createProduct();
+public interface ProductController extends EntityController<Product> {
+
 }
