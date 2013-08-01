67ce03cec67f13677f407a7c74a4f39c90256e59
Kapil Koju <kplx2.0@gmail.com>
2013-08-01 12:13:03 +0545

    Annotate with @Service
    OPEN - task 54: Create Category CRUD packages 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/54

--- OpERP/src/main/java/devopsdistilled/operp/server/data/service/items/impl/CategoryServiceImpl.java
+++ OpERP/src/main/java/devopsdistilled/operp/server/data/service/items/impl/CategoryServiceImpl.java
@@ -2,11 +2,14 @@ package devopsdistilled.operp.server.data.service.items.impl;
 
 import javax.inject.Inject;
 
+import org.springframework.stereotype.Service;
+
 import devopsdistilled.operp.server.data.entity.items.Category;
 import devopsdistilled.operp.server.data.repo.items.CategoryRepository;
 import devopsdistilled.operp.server.data.service.impl.AbstractEntityService;
 import devopsdistilled.operp.server.data.service.items.CategoryService;
 
+@Service
 public class CategoryServiceImpl extends
 		AbstractEntityService<Category, Long, CategoryRepository> implements
 		CategoryService {
