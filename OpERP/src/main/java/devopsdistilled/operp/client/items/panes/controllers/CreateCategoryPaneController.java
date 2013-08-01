db878088c1bb3d8f4027aef56156ca1234a2da3e
Kapil Koju <kplx2.0@gmail.com>
2013-08-01 12:51:47 +0545

    Declare validate() and save()
    OPEN - task 54: Create Category CRUD packages 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/54

--- OpERP/src/main/java/devopsdistilled/operp/client/items/panes/controllers/CreateCategoryPaneController.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/items/panes/controllers/CreateCategoryPaneController.java
@@ -1,7 +1,14 @@
 package devopsdistilled.operp.client.items.panes.controllers;
 
 import devopsdistilled.operp.client.abstracts.SubTaskPaneController;
+import devopsdistilled.operp.client.items.exceptions.EntityNameExistsException;
+import devopsdistilled.operp.client.items.exceptions.NullFieldException;
+import devopsdistilled.operp.server.data.entity.items.Category;
 
 public interface CreateCategoryPaneController extends SubTaskPaneController {
 
+	void validate(Category category) throws NullFieldException, EntityNameExistsException;
+
+	Category save(Category category);
+	
 }
