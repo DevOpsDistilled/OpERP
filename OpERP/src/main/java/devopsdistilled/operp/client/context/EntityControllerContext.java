6ed663019fe93f921c4a22f2bdb48d5dbeee3768
Kapil Koju <kplx2.0@gmail.com>
2013-08-01 12:21:32 +0545

    @Bean CategoryController
    OPEN - task 54: Create Category CRUD packages 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/54

--- OpERP/src/main/java/devopsdistilled/operp/client/context/EntityControllerContext.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/context/EntityControllerContext.java
@@ -3,13 +3,21 @@ package devopsdistilled.operp.client.context;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import devopsdistilled.operp.client.items.controllers.CategoryController;
+import devopsdistilled.operp.client.items.controllers.CategoryControllerImpl;
 import devopsdistilled.operp.client.items.controllers.ItemController;
 import devopsdistilled.operp.client.items.controllers.impl.ItemControllerImpl;
 
 @Configuration
 public class EntityControllerContext {
+
 	@Bean
 	public ItemController itemController() {
 		return new ItemControllerImpl();
+	}
+
+	@Bean
+	public CategoryController categoryController() {
+		return new CategoryControllerImpl();
 	}
 }
