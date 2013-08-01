b5a4e12026c8faf05c934f7950217c693ae4179e
Kapil Koju <kplx2.0@gmail.com>
2013-08-02 00:56:17 +0545

    Declare method updateCategories()
    OPEN - task 54: Create Category CRUD packages 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/54

--- OpERP/src/main/java/devopsdistilled/operp/client/items/models/observers/CategoryModelObserver.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/items/models/observers/CategoryModelObserver.java
@@ -1,7 +1,12 @@
 package devopsdistilled.operp.client.items.models.observers;
 
-import devopsdistilled.operp.client.abstracts.EntityModelObserver;
+import java.util.List;
 
-public interface CategoryModelObserver extends EntityModelObserver{
+import devopsdistilled.operp.client.abstracts.EntityModelObserver;
+import devopsdistilled.operp.server.data.entity.items.Category;
+
+public interface CategoryModelObserver extends EntityModelObserver {
+
+	public void updateCategories(List<Category> categories);
 
 }
