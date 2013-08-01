e49cae22e19830490149eaa62d8128f0968d54c7
Kapil Koju <kplx2.0@gmail.com>
2013-08-01 12:51:56 +0545

    Define validate()
    OPEN - task 54: Create Category CRUD packages 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/54

--- OpERP/src/main/java/devopsdistilled/operp/client/items/panes/controllers/impl/CreateCategoryPaneControllerImpl.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/items/panes/controllers/impl/CreateCategoryPaneControllerImpl.java
@@ -2,10 +2,13 @@ package devopsdistilled.operp.client.items.panes.controllers.impl;
 
 import javax.inject.Inject;
 
+import devopsdistilled.operp.client.items.exceptions.EntityNameExistsException;
+import devopsdistilled.operp.client.items.exceptions.NullFieldException;
 import devopsdistilled.operp.client.items.models.CategoryModel;
 import devopsdistilled.operp.client.items.panes.CreateCategoryPane;
 import devopsdistilled.operp.client.items.panes.controllers.CreateCategoryPaneController;
 import devopsdistilled.operp.client.items.panes.models.CreateCategoryPaneModel;
+import devopsdistilled.operp.server.data.entity.items.Category;
 
 public class CreateCategoryPaneControllerImpl implements
 		CreateCategoryPaneController {
@@ -23,6 +26,24 @@ public class CreateCategoryPaneControllerImpl implements
 	public void init() {
 		view.init();
 		model.registerObserver(view);
+	}
+
+	@Override
+	public void validate(Category category) throws NullFieldException, EntityNameExistsException {
+		if (category.getCategoryName().equalsIgnoreCase("")) {
+			throw new NullFieldException();
+		}
+
+		if (categoryModel.getService().isCategoryNameExists(
+				category.getCategoryName())) {
+			throw new EntityNameExistsException();
+		}
+	}
+
+	@Override
+	public Category save(Category category) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
