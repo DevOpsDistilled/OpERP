e6201c195faa8b08913804e22b8523fd1d14e9c3
Kapil Koju <kplx2.0@gmail.com>
2013-08-01 12:21:48 +0545

    @Bean for CreateCategoryPane
    OPEN - task 54: Create Category CRUD packages 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/54

--- OpERP/src/main/java/devopsdistilled/operp/client/context/MvcContext.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/context/MvcContext.java
@@ -3,24 +3,30 @@ package devopsdistilled.operp.client.context;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import devopsdistilled.operp.client.items.panes.CreateCategoryPane;
 import devopsdistilled.operp.client.items.panes.CreateItemPane;
 import devopsdistilled.operp.client.items.panes.EditItemPane;
 import devopsdistilled.operp.client.items.panes.ListItemPane;
+import devopsdistilled.operp.client.items.panes.controllers.CreateCategoryPaneController;
 import devopsdistilled.operp.client.items.panes.controllers.CreateItemPaneController;
 import devopsdistilled.operp.client.items.panes.controllers.EditItemPaneController;
 import devopsdistilled.operp.client.items.panes.controllers.ListItemPaneController;
+import devopsdistilled.operp.client.items.panes.controllers.impl.CreateCategoryPaneControllerImpl;
 import devopsdistilled.operp.client.items.panes.controllers.impl.CreateItemPaneControllerImpl;
 import devopsdistilled.operp.client.items.panes.controllers.impl.EditItemPaneControllerImpl;
 import devopsdistilled.operp.client.items.panes.controllers.impl.ListItemPaneControllerImpl;
-import devopsdistilled.operp.client.items.panes.model.CreateItemPaneModel;
-import devopsdistilled.operp.client.items.panes.model.EditItemPaneModel;
-import devopsdistilled.operp.client.items.panes.model.ListItemPaneModel;
-import devopsdistilled.operp.client.items.panes.model.impl.CreateItemPaneModelImpl;
-import devopsdistilled.operp.client.items.panes.model.impl.EditItemPaneModelImpl;
-import devopsdistilled.operp.client.items.panes.model.impl.ListItemPaneModelImpl;
+import devopsdistilled.operp.client.items.panes.models.CreateCategoryPaneModel;
+import devopsdistilled.operp.client.items.panes.models.CreateItemPaneModel;
+import devopsdistilled.operp.client.items.panes.models.EditItemPaneModel;
+import devopsdistilled.operp.client.items.panes.models.ListItemPaneModel;
+import devopsdistilled.operp.client.items.panes.models.impl.CreateCategoryPaneModelImpl;
+import devopsdistilled.operp.client.items.panes.models.impl.CreateItemPaneModelImpl;
+import devopsdistilled.operp.client.items.panes.models.impl.EditItemPaneModelImpl;
+import devopsdistilled.operp.client.items.panes.models.impl.ListItemPaneModelImpl;
 
 @Configuration
 public class MvcContext {
+
 	@Bean
 	public CreateItemPaneModel createItemPaneModel() {
 		return new CreateItemPaneModelImpl();
@@ -34,6 +40,11 @@ public class MvcContext {
 	@Bean
 	public EditItemPaneModel editItemPaneModel() {
 		return new EditItemPaneModelImpl();
+	}
+
+	@Bean
+	public CreateCategoryPaneModel createCategoryPaneModel() {
+		return new CreateCategoryPaneModelImpl();
 	}
 
 	@Bean
@@ -52,6 +63,11 @@ public class MvcContext {
 	}
 
 	@Bean
+	public CreateCategoryPane createCategoryPane() {
+		return new CreateCategoryPane();
+	}
+
+	@Bean
 	public CreateItemPaneController createItemPaneController() {
 		return new CreateItemPaneControllerImpl();
 	}
@@ -64,5 +80,10 @@ public class MvcContext {
 	@Bean
 	public EditItemPaneController editItemPaneController() {
 		return new EditItemPaneControllerImpl();
+	}
+
+	@Bean
+	public CreateCategoryPaneController createCategoryPaneController() {
+		return new CreateCategoryPaneControllerImpl();
 	}
 }
