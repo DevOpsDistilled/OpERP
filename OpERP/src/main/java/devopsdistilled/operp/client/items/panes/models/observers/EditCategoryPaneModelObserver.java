b79b4769e9ddae541ff8d120d5aaaf5e9b5208bf
Kapil Koju <kplx2.0@gmail.com>
2013-08-01 19:22:48 +0545

    Add observer for EditCategoryPane
    OPEN - task 54: Create Category CRUD packages 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/54

--- OpERP/src/main/java/devopsdistilled/operp/client/items/panes/models/observers/EditCategoryPaneModelObserver.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/items/panes/models/observers/EditCategoryPaneModelObserver.java
@@ -1,9 +1,9 @@
 package devopsdistilled.operp.client.items.panes.models.observers;
 
-import devopsdistilled.operp.client.abstracts.PaneModelObserver;
+import devopsdistilled.operp.client.abstracts.EditEntityPaneModelObserver;
 import devopsdistilled.operp.server.data.entity.items.Category;
 
-public interface EditCategoryPaneModelObserver extends PaneModelObserver {
+public interface EditCategoryPaneModelObserver extends
+		EditEntityPaneModelObserver<Category> {
 
-	public void updateCategory(Category category);
 }
