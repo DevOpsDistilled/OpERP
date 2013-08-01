5323a9b1a25ac8317b7863e8eff4111339273df4
Kapil Koju <kplx2.0@gmail.com>
2013-08-01 12:28:39 +0545

    Minor Changes: Refactoring
    OPEN - task 54: Create Category CRUD packages 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/54

--- OpERP/src/main/java/devopsdistilled/operp/client/items/panes/models/impl/CreateProductPaneModelImpl.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/items/panes/models/impl/CreateProductPaneModelImpl.java
@@ -1,7 +1,8 @@
-package devopsdistilled.operp.client.items.panes.models.observers;
+package devopsdistilled.operp.client.items.panes.models.impl;
 
 import devopsdistilled.operp.client.abstracts.AbstractPaneModel;
 import devopsdistilled.operp.client.items.panes.models.CreateProductPaneModel;
+import devopsdistilled.operp.client.items.panes.models.observers.CreateProductPaneModelObserver;
 
 public class CreateProductPaneModelImpl extends
 		AbstractPaneModel<CreateProductPaneModelObserver> implements
