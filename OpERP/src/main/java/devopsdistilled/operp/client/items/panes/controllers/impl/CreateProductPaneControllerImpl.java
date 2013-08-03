538c1f8eacf01ee1a906cb8dc49b50c8f80628a2
Kapil Koju <kplx2.0@gmail.com>
2013-08-03 12:35:01 +0545

    @Inject ProductModel
    OPEN - task 51: Create CRUD packages for Product entity 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/51

--- OpERP/src/main/java/devopsdistilled/operp/client/items/panes/controllers/impl/CreateProductPaneControllerImpl.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/items/panes/controllers/impl/CreateProductPaneControllerImpl.java
@@ -2,10 +2,14 @@ package devopsdistilled.operp.client.items.panes.controllers.impl;
 
 import javax.inject.Inject;
 
+import devopsdistilled.operp.client.items.exceptions.EntityNameExistsException;
+import devopsdistilled.operp.client.items.exceptions.NullFieldException;
 import devopsdistilled.operp.client.items.models.CategoryModel;
+import devopsdistilled.operp.client.items.models.ProductModel;
 import devopsdistilled.operp.client.items.panes.CreateProductPane;
 import devopsdistilled.operp.client.items.panes.controllers.CreateProductPaneController;
 import devopsdistilled.operp.client.items.panes.models.CreateProductPaneModel;
+import devopsdistilled.operp.server.data.entity.items.Product;
 
 public class CreateProductPaneControllerImpl implements
 		CreateProductPaneController {
@@ -19,11 +23,26 @@ public class CreateProductPaneControllerImpl implements
 	@Inject
 	private CategoryModel categoryModel;
 
+	@Inject
+	private ProductModel productModel;
+
 	@Override
 	public void init() {
 		view.init();
 		model.registerObserver(view);
 		categoryModel.registerObserver(view);
+	}
+
+	@Override
+	public void validate(Product product) throws NullFieldException,
+			EntityNameExistsException {
+
+	}
+
+	@Override
+	public Product save(Product product) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
