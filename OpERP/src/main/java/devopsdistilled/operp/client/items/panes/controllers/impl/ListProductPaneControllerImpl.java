7503ba8b9784158fdf50ad9a5b01f42f77538da8
Kapil Koju <kplx2.0@gmail.com>
2013-08-03 17:38:32 +0545

    @Inject ProductModel and registerObserver
    OPEN - task 51: Create CRUD packages for Product entity 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/51

--- OpERP/src/main/java/devopsdistilled/operp/client/items/panes/controllers/impl/ListProductPaneControllerImpl.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/items/panes/controllers/impl/ListProductPaneControllerImpl.java
@@ -2,6 +2,7 @@ package devopsdistilled.operp.client.items.panes.controllers.impl;
 
 import javax.inject.Inject;
 
+import devopsdistilled.operp.client.items.models.ProductModel;
 import devopsdistilled.operp.client.items.panes.ListProductPane;
 import devopsdistilled.operp.client.items.panes.controllers.ListProductPaneController;
 import devopsdistilled.operp.client.items.panes.models.ListProductPaneModel;
@@ -14,10 +15,14 @@ public class ListProductPaneControllerImpl implements ListProductPaneController 
 	@Inject
 	private ListProductPaneModel model;
 
+	@Inject
+	private ProductModel productModel;
+
 	@Override
 	public void init() {
 		view.init();
 		model.registerObserver(view);
+		productModel.registerObserver(view);
 	}
 
 }
