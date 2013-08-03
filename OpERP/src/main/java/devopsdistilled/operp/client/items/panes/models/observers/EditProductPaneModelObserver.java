2edfa7df5a68ae15627ea8ec5a71e7cf88b2401a
Kapil Koju <kplx2.0@gmail.com>
2013-08-03 19:12:15 +0545

    Add Observer for EditProductPane
    OPEN - task 51: Create CRUD packages for Product entity 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/51

---
+++ OpERP/src/main/java/devopsdistilled/operp/client/items/panes/models/observers/EditProductPaneModelObserver.java
@@ -0,0 +1,9 @@
+package devopsdistilled.operp.client.items.panes.models.observers;
+
+import devopsdistilled.operp.client.abstracts.EditEntityPaneModelObserver;
+import devopsdistilled.operp.server.data.entity.items.Product;
+
+public interface EditProductPaneModelObserver extends
+		EditEntityPaneModelObserver<Product> {
+
+}
