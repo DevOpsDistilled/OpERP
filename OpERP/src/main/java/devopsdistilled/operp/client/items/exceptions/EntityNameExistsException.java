7b30180f7f4c7eff7e8da89ba839ca2d71c5fa0c
Kapil Koju <kplx2.0@gmail.com>
2013-08-03 12:47:54 +0545

    Add constructors with message
    OPEN - task 51: Create CRUD packages for Product entity 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/51

--- OpERP/src/main/java/devopsdistilled/operp/client/items/exceptions/EntityNameExistsException.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/items/exceptions/EntityNameExistsException.java
@@ -4,4 +4,12 @@ public class EntityNameExistsException extends Exception {
 
 	private static final long serialVersionUID = 7326121173982471895L;
 
+	public EntityNameExistsException() {
+		super("Entity Name already exists");
+	}
+
+	public EntityNameExistsException(String message) {
+		super(message);
+	}
+
 }
