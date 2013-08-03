6df41ba0cc70c942fed838bcf1a0e5396d552695
Kapil Koju <kplx2.0@gmail.com>
2013-08-03 07:55:20 +0545

    @Bean ProductController
    OPEN - task 51: Create CRUD packages for Product entity 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/51

--- OpERP/src/main/java/devopsdistilled/operp/client/items/panes/CreateProductPane.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/items/panes/CreateProductPane.java
@@ -1,15 +1,52 @@
 package devopsdistilled.operp.client.items.panes;
 
+import javax.inject.Inject;
+import javax.swing.JButton;
 import javax.swing.JComponent;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
 
+import net.miginfocom.swing.MigLayout;
 import devopsdistilled.operp.client.abstracts.SubTaskPane;
+import devopsdistilled.operp.client.items.panes.controllers.CreateProductPaneController;
 import devopsdistilled.operp.client.items.panes.models.observers.CreateProductPaneModelObserver;
 
 public class CreateProductPane extends SubTaskPane implements
 		CreateProductPaneModelObserver {
 
-	private JPanel pane;
+	@Inject
+	private CreateProductPaneController controller;
+
+	private final JPanel pane;
+	private final JTextField productNameField;
+
+	public CreateProductPane() {
+		pane = new JPanel();
+		pane.setLayout(new MigLayout("", "[][grow]", "[][grow][]"));
+
+		JLabel lblProductName = new JLabel("Product Name");
+		pane.add(lblProductName, "cell 0 0,alignx trailing");
+
+		productNameField = new JTextField();
+		pane.add(productNameField, "cell 1 0,growx");
+		productNameField.setColumns(10);
+
+		JLabel lblCategory = new JLabel("Category");
+		pane.add(lblCategory, "cell 0 1");
+
+		JPanel categoryPanel = new JPanel();
+
+		JScrollPane scrollPane = new JScrollPane(categoryPanel);
+		pane.add(scrollPane, "cell 1 1,grow");
+
+		JButton btnCancel = new JButton("Cancel");
+		pane.add(btnCancel, "flowx,cell 1 2");
+
+		JButton btnCreate = new JButton("Create");
+		pane.add(btnCreate, "cell 1 2");
+	}
 
 	@Override
 	public JComponent getPane() {
