ff2aed35903f1c344c51c771cbb2b51c6948998b
Kapil Koju <kplx2.0@gmail.com>
2013-08-03 08:01:48 +0545

    @Inject view and model and define init()
    OPEN - task 51: Create CRUD packages for Product entity 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/51

--- OpERP/src/main/java/devopsdistilled/operp/client/items/panes/controllers/impl/CreateProductPaneControllerImpl.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/items/panes/controllers/impl/CreateProductPaneControllerImpl.java
@@ -1,13 +1,24 @@
 package devopsdistilled.operp.client.items.panes.controllers.impl;
 
+import javax.inject.Inject;
+
+import devopsdistilled.operp.client.items.panes.CreateProductPane;
 import devopsdistilled.operp.client.items.panes.controllers.CreateProductPaneController;
+import devopsdistilled.operp.client.items.panes.models.CreateProductPaneModel;
 
 public class CreateProductPaneControllerImpl implements
 		CreateProductPaneController {
 
+	@Inject
+	private CreateProductPane view;
+
+	@Inject
+	private CreateProductPaneModel model;
+
 	@Override
 	public void init() {
-		
+		view.init();
+		model.registerObserver(view);
 	}
 
 }
