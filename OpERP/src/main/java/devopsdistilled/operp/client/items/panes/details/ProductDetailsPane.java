a1e3a59ea60a117189fb54210171fdadeebc0bd3
Kapil Koju <kplx2.0@gmail.com>
2013-08-03 13:37:59 +0545

    Impl show() and dispose when "OK"
    OPEN - task 51: Create CRUD packages for Product entity 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/51

--- OpERP/src/main/java/devopsdistilled/operp/client/items/panes/details/ProductDetailsPane.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/items/panes/details/ProductDetailsPane.java
@@ -1,14 +1,71 @@
 package devopsdistilled.operp.client.items.panes.details;
 
-import javax.swing.JComponent;
-import javax.swing.JPanel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import net.miginfocom.swing.MigLayout;
 import devopsdistilled.operp.client.abstracts.AbstractEntityDetailsPane;
+import devopsdistilled.operp.server.data.entity.items.Category;
 import devopsdistilled.operp.server.data.entity.items.Product;
 
 public class ProductDetailsPane extends AbstractEntityDetailsPane<Product> {
 
-	private JPanel pane;
+	private Product product;
+
+	private final JPanel pane;
+	private final JTextField productIdField;
+	private final JTextField productNameField;
+	private final JList<Category> productCategoryList;
+
+	public ProductDetailsPane() {
+		pane = new JPanel();
+		pane.setLayout(new MigLayout("", "[][grow]", "[][][grow][]"));
+
+		JLabel lblProductId = new JLabel("Product ID");
+		pane.add(lblProductId, "cell 0 0,alignx trailing");
+
+		productIdField = new JTextField();
+		productIdField.setEditable(false);
+		pane.add(productIdField, "cell 1 0,growx");
+		productIdField.setColumns(10);
+
+		JLabel lblProductName = new JLabel("Product Name");
+		pane.add(lblProductName, "cell 0 1,alignx trailing");
+
+		productNameField = new JTextField();
+		productNameField.setEditable(false);
+		pane.add(productNameField, "cell 1 1,growx");
+		productNameField.setColumns(10);
+
+		JLabel lblProductCategory = new JLabel("Product Category");
+		pane.add(lblProductCategory, "cell 0 2");
+
+		productCategoryList = new JList();
+		pane.add(productCategoryList, "cell 1 2,grow");
+
+		JButton btnDelete = new JButton("Delete");
+		pane.add(btnDelete, "flowx,cell 1 3");
+
+		JButton btnEdit = new JButton("Edit");
+		pane.add(btnEdit, "cell 1 3");
+
+		JButton btnOk = new JButton("OK");
+		btnOk.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				getDialog().dispose();
+			}
+		});
+		btnOk.setSelected(true);
+		pane.add(btnOk, "cell 1 3");
+	}
 
 	@Override
 	public JComponent getPane() {
@@ -17,8 +74,15 @@ public class ProductDetailsPane extends AbstractEntityDetailsPane<Product> {
 
 	@Override
 	public void show(Product product) {
-		// TODO Auto-generated method stub
+		this.product = product;
+		if (product != null) {
+			productIdField.setText(product.getProductId().toString());
+			productNameField.setText(product.getProductName());
+			productCategoryList.setListData((Category[]) product
+					.getCategories().toArray());
 
+			getDialog().setVisible(true);
+		}
 	}
 
 	@Override
