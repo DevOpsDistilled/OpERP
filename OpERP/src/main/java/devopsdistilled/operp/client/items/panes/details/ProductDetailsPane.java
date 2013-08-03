d00c065b94636ff7e18833b581638e63e3288d46
Kapil Koju <kplx2.0@gmail.com>
2013-08-03 12:58:32 +0545

    Add bare ProductDetailsPane
    OPEN - task 51: Create CRUD packages for Product entity 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/51

---
+++ OpERP/src/main/java/devopsdistilled/operp/client/items/panes/details/ProductDetailsPane.java
@@ -0,0 +1,24 @@
+package devopsdistilled.operp.client.items.panes.details;
+
+import javax.swing.JComponent;
+import javax.swing.JPanel;
+
+import devopsdistilled.operp.client.abstracts.AbstractEntityDetailsPane;
+import devopsdistilled.operp.server.data.entity.items.Product;
+
+public class ProductDetailsPane extends AbstractEntityDetailsPane<Product> {
+
+	private JPanel pane;
+
+	@Override
+	public JComponent getPane() {
+		return pane;
+	}
+
+	@Override
+	public void show(Product product) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
