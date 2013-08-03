8dc1ae6c09cbaec369e3f19fef14ec3b968a3fe6
Kapil Koju <kplx2.0@gmail.com>
2013-08-03 17:36:28 +0545

    Add Controller for ListProductPane
    OPEN - task 51: Create CRUD packages for Product entity 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/51

---
+++ OpERP/src/main/java/devopsdistilled/operp/client/items/panes/controllers/impl/ListProductPaneControllerImpl.java
@@ -0,0 +1,15 @@
+package devopsdistilled.operp.client.items.panes.controllers.impl;
+
+import devopsdistilled.operp.client.items.panes.controllers.ListProductPaneController;
+
+public class ListProductPaneControllerImpl implements ListProductPaneController {
+
+	
+	
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
