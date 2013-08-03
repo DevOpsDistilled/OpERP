06aa08f11f613440a00a4d47d50dfb15f7ef0013
Kapil Koju <kplx2.0@gmail.com>
2013-08-03 19:40:54 +0545

    @Inject controller
    OPEN - task 51: Create CRUD packages for Product entity 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/51

--- OpERP/src/main/java/devopsdistilled/operp/client/items/panes/EditProductPane.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/items/panes/EditProductPane.java
@@ -1,14 +1,19 @@
 package devopsdistilled.operp.client.items.panes;
 
+import javax.inject.Inject;
 import javax.swing.JComponent;
 import javax.swing.JPanel;
 
 import devopsdistilled.operp.client.abstracts.SubTaskPane;
+import devopsdistilled.operp.client.items.panes.controllers.EditProductPaneController;
 import devopsdistilled.operp.client.items.panes.models.observers.EditProductPaneModelObserver;
 import devopsdistilled.operp.server.data.entity.items.Product;
 
 public class EditProductPane extends SubTaskPane implements
 		EditProductPaneModelObserver {
+
+	@Inject
+	private EditProductPaneController controller;
 
 	private final JPanel pane;
 
