3c64777b592c6a41438ee074e11789b2b7eade74
Kapil Koju <kplx2.0@gmail.com>
2013-08-04 19:02:46 +0545

    @Service ManufacturerServiceImpl

--- OpERP/src/main/java/devopsdistilled/operp/server/data/service/items/impl/ManufacturerServiceImpl.java
+++ OpERP/src/main/java/devopsdistilled/operp/server/data/service/items/impl/ManufacturerServiceImpl.java
@@ -2,11 +2,14 @@ package devopsdistilled.operp.server.data.service.items.impl;
 
 import javax.inject.Inject;
 
+import org.springframework.stereotype.Service;
+
 import devopsdistilled.operp.server.data.entity.items.Manufacturer;
 import devopsdistilled.operp.server.data.repo.items.ManufacturerRepository;
 import devopsdistilled.operp.server.data.service.impl.AbstractEntityService;
 import devopsdistilled.operp.server.data.service.items.ManufacturerService;
 
+@Service
 public class ManufacturerServiceImpl extends
 		AbstractEntityService<Manufacturer, Long, ManufacturerRepository>
 		implements ManufacturerService {
