c879073310a393853f83d267f63b2f2c7577283c
Kapil Koju <kplx2.0@gmail.com>
2013-08-04 19:01:30 +0545

    Declare and Define bare ManufacturerService

---
+++ OpERP/src/main/java/devopsdistilled/operp/server/data/service/items/ManufacturerService.java
@@ -0,0 +1,8 @@
+package devopsdistilled.operp.server.data.service.items;
+
+import devopsdistilled.operp.server.data.entity.items.Manufacturer;
+import devopsdistilled.operp.server.data.service.EntityService;
+
+public interface ManufacturerService extends EntityService<Manufacturer, Long> {
+
+}
---
+++ OpERP/src/main/java/devopsdistilled/operp/server/data/service/items/impl/ManufacturerServiceImpl.java
@@ -0,0 +1,24 @@
+package devopsdistilled.operp.server.data.service.items.impl;
+
+import javax.inject.Inject;
+
+import devopsdistilled.operp.server.data.entity.items.Manufacturer;
+import devopsdistilled.operp.server.data.repo.items.ManufacturerRepository;
+import devopsdistilled.operp.server.data.service.impl.AbstractEntityService;
+import devopsdistilled.operp.server.data.service.items.ManufacturerService;
+
+public class ManufacturerServiceImpl extends
+		AbstractEntityService<Manufacturer, Long, ManufacturerRepository>
+		implements ManufacturerService {
+
+	private static final long serialVersionUID = -6005274349296129598L;
+
+	@Inject
+	private ManufacturerRepository repo;
+
+	@Override
+	protected ManufacturerRepository getRepo() {
+		return repo;
+	}
+
+}
