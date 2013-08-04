f8da1ddfe1b70878c4151ce62b2ceacc83efff2c
Kapil Koju <kplx2.0@gmail.com>
2013-08-04 19:41:56 +0545

    Create bare components for ManufacturerModel

---
+++ OpERP/src/main/java/devopsdistilled/operp/client/items/models/observers/ManufacturerModelObserver.java
@@ -0,0 +1,7 @@
+package devopsdistilled.operp.client.items.models.observers;
+
+import devopsdistilled.operp.client.abstracts.EntityModelObserver;
+
+public interface ManufacturerModelObserver extends EntityModelObserver {
+
+}
