4fbe6d9833ab5d5cce76f318255e918c8519803d
Kapil Koju <kplx2.0@gmail.com>
2013-08-04 19:37:05 +0545

    @Inject view and model and Impl init()

--- OpERP/src/main/java/devopsdistilled/operp/client/items/panes/controllers/impl/CreateManufacturerPaneControllerImpl.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/items/panes/controllers/impl/CreateManufacturerPaneControllerImpl.java
@@ -1,12 +1,22 @@
 package devopsdistilled.operp.client.items.panes.controllers.impl;
 
+import javax.inject.Inject;
+
 import devopsdistilled.operp.client.items.exceptions.EntityNameExistsException;
 import devopsdistilled.operp.client.items.exceptions.NullFieldException;
+import devopsdistilled.operp.client.items.panes.CreateManufacturerPane;
 import devopsdistilled.operp.client.items.panes.controllers.CreateManufacturerPaneController;
+import devopsdistilled.operp.client.items.panes.models.CreateManufacturerPaneModel;
 import devopsdistilled.operp.server.data.entity.items.Manufacturer;
 
 public class CreateManufacturerPaneControllerImpl implements
 		CreateManufacturerPaneController {
+
+	@Inject
+	private CreateManufacturerPane view;
+
+	@Inject
+	private CreateManufacturerPaneModel model;
 
 	@Override
 	public void validate(Manufacturer entity) throws NullFieldException,
@@ -23,8 +33,8 @@ public class CreateManufacturerPaneControllerImpl implements
 
 	@Override
 	public void init() {
-		// TODO Auto-generated method stub
-
+		view.init();
+		model.registerObserver(view);
 	}
 
 }
