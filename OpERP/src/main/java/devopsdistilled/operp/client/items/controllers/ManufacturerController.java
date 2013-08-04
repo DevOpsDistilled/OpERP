881e719d60ad3eb000194f17a27ba2a604d122df
Kapil Koju <kplx2.0@gmail.com>
2013-08-04 20:05:22 +0545

    Create bare ManufacturerController

---
+++ OpERP/src/main/java/devopsdistilled/operp/client/items/controllers/ManufacturerController.java
@@ -0,0 +1,8 @@
+package devopsdistilled.operp.client.items.controllers;
+
+import devopsdistilled.operp.client.abstracts.EntityController;
+import devopsdistilled.operp.server.data.entity.items.Manufacturer;
+
+public interface ManufacturerController extends EntityController<Manufacturer> {
+
+}
---
+++ OpERP/src/main/java/devopsdistilled/operp/client/items/controllers/impl/ManufacturerControllerImpl.java
@@ -0,0 +1,32 @@
+package devopsdistilled.operp.client.items.controllers.impl;
+
+import devopsdistilled.operp.client.items.controllers.ManufacturerController;
+import devopsdistilled.operp.server.data.entity.items.Manufacturer;
+
+public class ManufacturerControllerImpl implements ManufacturerController {
+
+	@Override
+	public void create() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void edit(Manufacturer entity) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void list() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void delete(Manufacturer entity) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
