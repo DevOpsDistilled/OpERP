749bfa03da392153cf5edfec3adb7ca3cbf0f602
Kapil Koju <kplx2.0@gmail.com>
2013-08-04 20:33:55 +0545

    Create basic layout and Impl "OK"

--- OpERP/src/main/java/devopsdistilled/operp/client/items/panes/details/ManufacturerDetailsPane.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/items/panes/details/ManufacturerDetailsPane.java
@@ -1,8 +1,15 @@
 package devopsdistilled.operp.client.items.panes.details;
 
-import javax.swing.JComponent;
-import javax.swing.JPanel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import net.miginfocom.swing.MigLayout;
 import devopsdistilled.operp.client.abstracts.AbstractEntityDetailsPane;
 import devopsdistilled.operp.server.data.entity.items.Manufacturer;
 
@@ -10,9 +17,51 @@ public class ManufacturerDetailsPane extends
 		AbstractEntityDetailsPane<Manufacturer> {
 
 	private final JPanel pane;
+	private final JTextField manufacturerIdField;
+	private final JTextField manufacturerNameField;
 
 	public ManufacturerDetailsPane() {
 		pane = new JPanel();
+		pane.setLayout(new MigLayout("", "[][grow]", "[][][][]"));
+
+		JLabel lblManufacturerId = new JLabel("Manufacturer ID");
+		pane.add(lblManufacturerId, "cell 0 0,alignx trailing");
+
+		manufacturerIdField = new JTextField();
+		pane.add(manufacturerIdField, "cell 1 0,growx");
+		manufacturerIdField.setColumns(10);
+
+		JLabel lblManufacturerName = new JLabel("Manufacturer Name");
+		pane.add(lblManufacturerName, "cell 0 1,alignx trailing");
+
+		manufacturerNameField = new JTextField();
+		pane.add(manufacturerNameField, "cell 1 1,growx");
+		manufacturerNameField.setColumns(10);
+
+		JButton btnDelete = new JButton("Delete");
+		btnDelete.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		pane.add(btnDelete, "flowx,cell 1 3");
+
+		JButton btnEdit = new JButton("Edit");
+		btnEdit.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		pane.add(btnEdit, "cell 1 3");
+
+		JButton btnOk = new JButton("OK");
+		btnOk.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				getDialog().dispose();
+			}
+		});
+		pane.add(btnOk, "cell 1 3");
 	}
 
 	@Override
