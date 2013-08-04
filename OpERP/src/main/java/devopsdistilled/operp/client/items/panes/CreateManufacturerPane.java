787d0ca042e0170520bb38f527d70bdd921d7621
Kapil Koju <kplx2.0@gmail.com>
2013-08-04 19:32:26 +0545

    @Inject controller and Impl "Create"

--- OpERP/src/main/java/devopsdistilled/operp/client/items/panes/CreateManufacturerPane.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/items/panes/CreateManufacturerPane.java
@@ -1,16 +1,69 @@
 package devopsdistilled.operp.client.items.panes;
 
-import javax.swing.JComponent;
-import javax.swing.JPanel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.inject.Inject;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import net.miginfocom.swing.MigLayout;
 import devopsdistilled.operp.client.abstracts.SubTaskPane;
+import devopsdistilled.operp.client.items.exceptions.EntityNameExistsException;
+import devopsdistilled.operp.client.items.exceptions.NullFieldException;
+import devopsdistilled.operp.client.items.panes.controllers.CreateManufacturerPaneController;
+import devopsdistilled.operp.server.data.entity.items.Manufacturer;
 
 public class CreateManufacturerPane extends SubTaskPane {
 
+	@Inject
+	private CreateManufacturerPaneController controller;
+
 	private final JPanel pane;
+	private final JTextField manufacturerNameField;
 
 	public CreateManufacturerPane() {
 		pane = new JPanel();
+		pane.setLayout(new MigLayout("", "[][grow]", "[][]"));
+
+		JLabel lblManufacturerName = new JLabel("Manufacturer Name");
+		pane.add(lblManufacturerName, "cell 0 0,alignx trailing");
+
+		manufacturerNameField = new JTextField();
+		pane.add(manufacturerNameField, "cell 1 0,growx");
+		manufacturerNameField.setColumns(10);
+
+		JButton btnCancel = new JButton("Cancel");
+		btnCancel.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				getDialog().dispose();
+			}
+		});
+		pane.add(btnCancel, "flowx,cell 1 1");
+
+		JButton btnCreate = new JButton("Create");
+		btnCreate.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Manufacturer manufacturer = new Manufacturer();
+				String manufactuerName = manufacturerNameField.getText().trim();
+				manufacturer.setManufacturerName(manufactuerName);
+
+				try {
+					controller.validate(manufacturer);
+					manufacturer = controller.save(manufacturer);
+
+				} catch (NullFieldException | EntityNameExistsException e1) {
+					JOptionPane.showMessageDialog(getPane(), e1.getMessage());
+				}
+			}
+		});
+		pane.add(btnCreate, "cell 1 1");
 	}
 
 	@Override
