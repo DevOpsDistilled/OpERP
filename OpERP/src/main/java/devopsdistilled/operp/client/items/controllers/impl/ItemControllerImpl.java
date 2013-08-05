4000e6ceba40b6556eaa1bdbd011569fefa21e2f
Kapil Koju <kplx2.0@gmail.com>
2013-08-06 00:34:46 +0545

    Impl ItemController
    OPEN - task 66: Update Item functionalities implementation 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/66

--- OpERP/src/main/java/devopsdistilled/operp/client/items/controllers/impl/ItemControllerImpl.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/items/controllers/impl/ItemControllerImpl.java
@@ -2,8 +2,6 @@ package devopsdistilled.operp.client.items.controllers.impl;
 
 import javax.inject.Inject;
 
-import org.springframework.context.ApplicationContext;
-
 import devopsdistilled.operp.client.items.controllers.ItemController;
 import devopsdistilled.operp.client.items.models.ItemModel;
 import devopsdistilled.operp.client.items.panes.controllers.CreateItemPaneController;
@@ -14,34 +12,34 @@ import devopsdistilled.operp.server.data.entity.items.Item;
 public class ItemControllerImpl implements ItemController {
 
 	@Inject
-	private ApplicationContext context;
+	private CreateItemPaneController createItemPaneController;
+
+	@Inject
+	private ListItemPaneController listItemPaneController;
+
+	@Inject
+	private EditItemPaneController editItemPaneController;
 
 	@Inject
 	private ItemModel itemModel;
 
 	@Override
-	public void createItem() {
-		CreateItemPaneController createItemPaneController = context
-				.getBean(CreateItemPaneController.class);
+	public void create() {
 		createItemPaneController.init();
 	}
 
 	@Override
-	public void listItems() {
-		ListItemPaneController listItemPaneController = context
-				.getBean(ListItemPaneController.class);
-		listItemPaneController.init();
-	}
-
-	@Override
-	public void editItem(Item item) {
-		EditItemPaneController editItemPaneController = context
-				.getBean(EditItemPaneController.class);
+	public void edit(Item item) {
 		editItemPaneController.init(item);
 	}
 
 	@Override
-	public void deleteItem(Item item) {
+	public void list() {
+		listItemPaneController.init();
+	}
+
+	@Override
+	public void delete(Item item) {
 		itemModel.deleteAndUpdateModel(item);
 	}
 
