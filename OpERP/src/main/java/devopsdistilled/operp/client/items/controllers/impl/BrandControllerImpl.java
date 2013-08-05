6b19a99d19b5cd41eaed4586d7831b80924a3abe
Kapil Koju <kplx2.0@gmail.com>
2013-08-05 17:00:52 +0545

    Create Controller for Brand
    OPEN - task 64: Create CRUD functionality for Brand entity 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/64

---
+++ OpERP/src/main/java/devopsdistilled/operp/client/items/controllers/impl/BrandControllerImpl.java
@@ -0,0 +1,32 @@
+package devopsdistilled.operp.client.items.controllers.impl;
+
+import devopsdistilled.operp.client.items.controllers.BrandController;
+import devopsdistilled.operp.server.data.entity.items.Brand;
+
+public class BrandControllerImpl implements BrandController {
+
+	@Override
+	public void create() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void edit(Brand entity) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void list() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void delete(Brand entity) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
