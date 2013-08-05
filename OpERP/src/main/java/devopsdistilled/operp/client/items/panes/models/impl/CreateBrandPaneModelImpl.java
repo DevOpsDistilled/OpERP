482bf8130fd2bc50072e2ded5db747f1ea06dba2
Kapil Koju <kplx2.0@gmail.com>
2013-08-05 17:10:27 +0545

    Implement Model
    OPEN - task 64: Create CRUD functionality for Brand entity 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/64

--- OpERP/src/main/java/devopsdistilled/operp/client/items/panes/models/impl/CreateBrandPaneModelImpl.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/items/panes/models/impl/CreateBrandPaneModelImpl.java
@@ -1,10 +1,12 @@
 package devopsdistilled.operp.client.items.panes.models.impl;
 
 import devopsdistilled.operp.client.abstracts.AbstractPaneModel;
+import devopsdistilled.operp.client.items.panes.models.CreateBrandPaneModel;
 import devopsdistilled.operp.client.items.panes.models.observers.CreateBrandPaneModelObserver;
 
 public class CreateBrandPaneModelImpl extends
-		AbstractPaneModel<CreateBrandPaneModelObserver> {
+		AbstractPaneModel<CreateBrandPaneModelObserver> implements
+		CreateBrandPaneModel {
 
 	@Override
 	public String getTitle() {
