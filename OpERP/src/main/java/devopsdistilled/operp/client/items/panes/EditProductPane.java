ca4cb0b2aa8f078c654e7ba57fb7ad571d139033
Kapil Koju <kplx2.0@gmail.com>
2013-08-06 01:51:08 +0545

    Impl validate() with single catch for Edit panes
    OPEN - task 66: Update Item functionalities implementation 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/66

--- OpERP/src/main/java/devopsdistilled/operp/client/items/panes/EditProductPane.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/items/panes/EditProductPane.java
@@ -20,8 +20,6 @@ import javax.swing.JTextField;
 import net.miginfocom.swing.MigLayout;
 import devopsdistilled.operp.client.abstracts.SubTaskPane;
 import devopsdistilled.operp.client.items.controllers.CategoryController;
-import devopsdistilled.operp.client.items.exceptions.EntityNameExistsException;
-import devopsdistilled.operp.client.items.exceptions.NullFieldException;
 import devopsdistilled.operp.client.items.models.observers.CategoryModelObserver;
 import devopsdistilled.operp.client.items.panes.controllers.EditProductPaneController;
 import devopsdistilled.operp.client.items.panes.details.ProductDetailsPane;
@@ -121,9 +119,7 @@ public class EditProductPane extends SubTaskPane implements
 					getDialog().dispose();
 					productDetailsPane.show(product);
 
-				} catch (NullFieldException e1) {
-					JOptionPane.showMessageDialog(getPane(), e1.getMessage());
-				} catch (EntityNameExistsException e1) {
+				} catch (Exception e1) {
 					JOptionPane.showMessageDialog(getPane(), e1.getMessage());
 
 				}
