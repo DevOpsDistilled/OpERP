5fdb9af896400e076015c65da8e805c82ab6f0ce
Kapil Koju <kplx2.0@gmail.com>
2013-08-05 17:28:20 +0545

    Impl bare validate() and save()
    OPEN - task 64: Create CRUD functionality for Brand entity 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/64

--- OpERP/src/main/java/devopsdistilled/operp/client/items/panes/controllers/impl/CreateBrandPaneControllerImpl.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/items/panes/controllers/impl/CreateBrandPaneControllerImpl.java
@@ -2,10 +2,13 @@ package devopsdistilled.operp.client.items.panes.controllers.impl;
 
 import javax.inject.Inject;
 
+import devopsdistilled.operp.client.items.exceptions.EntityNameExistsException;
+import devopsdistilled.operp.client.items.exceptions.NullFieldException;
 import devopsdistilled.operp.client.items.models.ManufacturerModel;
 import devopsdistilled.operp.client.items.panes.CreateBrandPane;
 import devopsdistilled.operp.client.items.panes.controllers.CreateBrandPaneController;
 import devopsdistilled.operp.client.items.panes.models.CreateBrandPaneModel;
+import devopsdistilled.operp.server.data.entity.items.Brand;
 
 public class CreateBrandPaneControllerImpl implements CreateBrandPaneController {
 
@@ -23,6 +26,18 @@ public class CreateBrandPaneControllerImpl implements CreateBrandPaneController 
 		view.init();
 		manufacturerModel.registerObserver(view);
 		model.registerObserver(view);
+	}
+
+	@Override
+	public void validate(Brand brand) throws NullFieldException,
+			EntityNameExistsException {
+
+	}
+
+	@Override
+	public Brand save(Brand brand) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
