940344bc50b8288020b404e95a3c4e3bcc6806c2
Kapil Koju <kplx2.0@gmail.com>
2013-08-05 23:04:58 +0545

    Create basic edit pane layout
    OPEN - task 64: Create CRUD functionality for Brand entity 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/64

--- OpERP/src/main/java/devopsdistilled/operp/client/items/panes/EditBrandPane.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/items/panes/EditBrandPane.java
@@ -1,10 +1,17 @@
 package devopsdistilled.operp.client.items.panes;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.List;
 
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JComponent;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
+import net.miginfocom.swing.MigLayout;
 import devopsdistilled.operp.client.abstracts.SubTaskPane;
 import devopsdistilled.operp.client.items.models.observers.ManufacturerModelObserver;
 import devopsdistilled.operp.client.items.panes.models.observers.EditBrandPaneModelObserver;
@@ -15,9 +22,50 @@ public class EditBrandPane extends SubTaskPane implements
 		EditBrandPaneModelObserver, ManufacturerModelObserver {
 
 	private final JPanel pane;
+	private final JTextField brandIdField;
+	private final JTextField brandNameField;
+	private final JComboBox<Manufacturer> manufacturersCombo;
 
 	public EditBrandPane() {
 		pane = new JPanel();
+		pane.setLayout(new MigLayout("", "[][grow]", "[][][][][]"));
+
+		JLabel lblBrandId = new JLabel("Brand ID");
+		pane.add(lblBrandId, "cell 0 0,alignx trailing");
+
+		brandIdField = new JTextField();
+		brandIdField.setEditable(false);
+		pane.add(brandIdField, "cell 1 0,growx");
+		brandIdField.setColumns(10);
+
+		JLabel lblBrandName = new JLabel("Brand Name");
+		pane.add(lblBrandName, "cell 0 1,alignx trailing");
+
+		brandNameField = new JTextField();
+		pane.add(brandNameField, "cell 1 1,growx");
+		brandNameField.setColumns(10);
+
+		JLabel lblManufacturer = new JLabel("Manufacturer");
+		pane.add(lblManufacturer, "cell 0 2,alignx trailing");
+
+		manufacturersCombo = new JComboBox<>();
+		pane.add(manufacturersCombo, "cell 1 2,growx");
+
+		JButton btnCancel = new JButton("Cancel");
+		btnCancel.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		pane.add(btnCancel, "flowx,cell 1 4");
+
+		JButton btnUpdate = new JButton("Update");
+		btnUpdate.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		pane.add(btnUpdate, "cell 1 4");
 	}
 
 	@Override
