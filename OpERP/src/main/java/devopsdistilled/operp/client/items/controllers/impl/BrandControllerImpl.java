aa6a16f33f8b748e80700a73a4d5a7583dbb252b
Kapil Koju <kplx2.0@gmail.com>
2013-08-05 17:10:38 +0545

    Inject and Impl create()
    OPEN - task 64: Create CRUD functionality for Brand entity 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/64

--- OpERP/src/main/java/devopsdistilled/operp/client/items/controllers/impl/BrandControllerImpl.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/items/controllers/impl/BrandControllerImpl.java
@@ -1,14 +1,19 @@
 package devopsdistilled.operp.client.items.controllers.impl;
 
+import javax.inject.Inject;
+
 import devopsdistilled.operp.client.items.controllers.BrandController;
+import devopsdistilled.operp.client.items.panes.CreateBrandPane;
 import devopsdistilled.operp.server.data.entity.items.Brand;
 
 public class BrandControllerImpl implements BrandController {
 
+	@Inject
+	private CreateBrandPane createBrandPane;
+
 	@Override
 	public void create() {
-		// TODO Auto-generated method stub
-
+		createBrandPane.init();
 	}
 
 	@Override
