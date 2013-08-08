473a33bd737dab4223b4f3586f07c151669ceabb
Kapil Koju <kplx2.0@gmail.com>
2013-08-08 19:50:54 +0545

    Bean transactionManager Proxy
    OPEN - task 70: Can't persist Item due to PersistentObjectException 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/70

--- OpERP/src/main/java/devopsdistilled/operp/client/context/AppContext.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/context/AppContext.java
@@ -1,7 +1,10 @@
 package devopsdistilled.operp.client.context;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.remoting.rmi.RmiProxyFactoryBean;
 
 import devopsdistilled.operp.client.context.items.ItemContext;
 
@@ -9,4 +12,11 @@ import devopsdistilled.operp.client.context.items.ItemContext;
 @Import({ ItemContext.class })
 public class AppContext {
 
+	@Bean
+	public RmiProxyFactoryBean transactionManager() {
+		RmiProxyFactoryBean rmiProxy = new RmiProxyFactoryBean();
+		rmiProxy.setServiceUrl("rmi://127.0.1.1:1099/TransactionManager");
+		rmiProxy.setServiceInterface(JpaTransactionManager.class);
+		return rmiProxy;
+	}
 }
