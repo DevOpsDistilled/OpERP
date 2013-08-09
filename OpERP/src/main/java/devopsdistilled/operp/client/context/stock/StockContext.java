9be157888c3977031be9d21ccc20a4cf0e2f50e9
Kapil Koju <kplx2.0@gmail.com>
2013-08-09 20:21:26 +0545

    Remove LocalContext from modules with Module Context

--- OpERP/src/main/java/devopsdistilled/operp/client/context/stock/StockContext.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/context/stock/StockContext.java
@@ -3,8 +3,12 @@ package devopsdistilled.operp.client.context.stock;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
+import devopsdistilled.operp.client.context.MainWindowContext;
+
 @Configuration
-@Import({ LocalContext.class })
+@Import({ MainWindowContext.class, EntityModelContext.class,
+		EntityControllerContext.class, MvcContext.class,
+		EntityDetailsContext.class })
 public class StockContext {
 
 }
