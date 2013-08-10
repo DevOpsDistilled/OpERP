4a8116baa78b5efe5bc8c695a9485399adcc4d20
Kapil Koju <kplx2.0@gmail.com>
2013-08-11 00:17:54 +0545

    Inject PaneController instead of ApplicationContext
    OPEN - task 24: Create a stock module 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/24

--- OpERP/src/main/java/devopsdistilled/operp/client/stock/controllers/impl/StockControllerImpl.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/stock/controllers/impl/StockControllerImpl.java
@@ -2,28 +2,26 @@ package devopsdistilled.operp.client.stock.controllers.impl;
 
 import javax.inject.Inject;
 
-import org.springframework.context.ApplicationContext;
-
 import devopsdistilled.operp.client.stock.controllers.StockController;
 import devopsdistilled.operp.client.stock.models.StockModel;
-import devopsdistilled.operp.client.stock.panes.controllers.UpdateStockPaneController;
 import devopsdistilled.operp.client.stock.panes.controllers.ListStockPaneController;
+import devopsdistilled.operp.client.stock.panes.controllers.UpdateStockPaneController;
 import devopsdistilled.operp.server.data.entity.stock.Stock;
 
 public class StockControllerImpl implements StockController {
 
 	@Inject
-	private ApplicationContext context;
+	private UpdateStockPaneController updateStockPaneController;
+
+	@Inject
+	private ListStockPaneController listStockPaneController;
 
 	@Inject
 	private StockModel stockModel;
 
 	@Override
 	public void create() {
-		UpdateStockPaneController createStockPaneController = context
-				.getBean(UpdateStockPaneController.class);
-		createStockPaneController.init();
-
+		updateStockPaneController.init();
 	}
 
 	@Override
@@ -34,14 +32,17 @@ public class StockControllerImpl implements StockController {
 
 	@Override
 	public void list() {
-		ListStockPaneController listStockPaneController = context
-				.getBean(ListStockPaneController.class);
 		listStockPaneController.init();
 	}
 
 	@Override
 	public void delete(Stock stock) {
 		stockModel.deleteAndUpdateModel(stock);
+	}
+
+	@Override
+	public void transfer() {
+		// TODO Auto-generated method stub
 
 	}
 
