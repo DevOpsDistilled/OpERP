7d71417f7035087929c982aee4fe9c108432173c
Kapil Koju <kplx2.0@gmail.com>
2013-08-11 01:06:55 +0545

    Declare throws clause
    OPEN - task 24: Create a stock module 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/24

--- OpERP/src/main/java/devopsdistilled/operp/client/stock/panes/controllers/TransferStockPaneController.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/stock/panes/controllers/TransferStockPaneController.java
@@ -1,12 +1,13 @@
 package devopsdistilled.operp.client.stock.panes.controllers;
 
 import devopsdistilled.operp.client.abstracts.SubTaskPaneController;
+import devopsdistilled.operp.client.exceptions.EntityValidationException;
 import devopsdistilled.operp.server.data.entity.stock.Warehouse;
 
 public interface TransferStockPaneController extends SubTaskPaneController {
 
 	public void validate(Warehouse fromWarehouse, Warehouse toWarehouse,
-			Long quantity);
+			Long quantity) throws EntityValidationException;
 
 	public void transfer(Warehouse fromWarehouse, Warehouse toWarehouse,
 			Long quantity);
