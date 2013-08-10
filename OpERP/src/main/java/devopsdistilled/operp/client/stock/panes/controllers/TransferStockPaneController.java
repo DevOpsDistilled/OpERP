e9c6c8b18636909bc03a3029da3080f0df456a0b
Kapil Koju <kplx2.0@gmail.com>
2013-08-11 01:01:35 +0545

    Declare validate() & transfer()
    OPEN - task 24: Create a stock module 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/24

--- OpERP/src/main/java/devopsdistilled/operp/client/stock/panes/controllers/TransferStockPaneController.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/stock/panes/controllers/TransferStockPaneController.java
@@ -1,7 +1,14 @@
 package devopsdistilled.operp.client.stock.panes.controllers;
 
 import devopsdistilled.operp.client.abstracts.SubTaskPaneController;
+import devopsdistilled.operp.server.data.entity.stock.Warehouse;
 
 public interface TransferStockPaneController extends SubTaskPaneController {
+
+	public void validate(Warehouse fromWarehouse, Warehouse toWarehouse,
+			Long quantity);
+
+	public void transfer(Warehouse fromWarehouse, Warehouse toWarehouse,
+			Long quantity);
 
 }
