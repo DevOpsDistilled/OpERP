84d61eb8e542f58e9522aeaa91c568916029e811
Kapil Koju <kplx2.0@gmail.com>
2013-08-11 19:53:15 +0545

    Create bare StockKeeper Controller
    OPEN - task 24: Create a stock module 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/24

---
+++ OpERP/src/main/java/devopsdistilled/operp/client/stock/controllers/impl/StockKeeperControllerImpl.java
@@ -0,0 +1,32 @@
+package devopsdistilled.operp.client.stock.controllers.impl;
+
+import devopsdistilled.operp.client.stock.controllers.StockKeeperController;
+import devopsdistilled.operp.server.data.entity.stock.StockKeeper;
+
+public class StockKeeperControllerImpl implements StockKeeperController {
+
+	@Override
+	public void create() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void edit(StockKeeper entity) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void list() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void delete(StockKeeper entity) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
