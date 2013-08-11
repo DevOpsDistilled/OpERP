a02a860afab064b14f79b2f8faae4469abb3a4e1
Kapil Koju <kplx2.0@gmail.com>
2013-08-11 19:56:27 +0545

    Create StockKeeper ModelObserver
    OPEN - task 24: Create a stock module 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/24

---
+++ OpERP/src/main/java/devopsdistilled/operp/client/stock/models/observers/StockKeeperModelObserver.java
@@ -0,0 +1,7 @@
+package devopsdistilled.operp.client.stock.models.observers;
+
+import devopsdistilled.operp.client.abstracts.EntityModelObserver;
+
+public interface StockKeeperModelObserver extends EntityModelObserver {
+
+}
