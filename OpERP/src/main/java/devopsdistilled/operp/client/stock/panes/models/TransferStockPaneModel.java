a4e867f21c275d2106d4d4acc4e9389269de6828
Kapil Koju <kplx2.0@gmail.com>
2013-08-11 14:54:49 +0545

    Declare methods to set View state
    OPEN - task 24: Create a stock module 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/24

--- OpERP/src/main/java/devopsdistilled/operp/client/stock/panes/models/TransferStockPaneModel.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/stock/panes/models/TransferStockPaneModel.java
@@ -2,8 +2,26 @@ package devopsdistilled.operp.client.stock.panes.models;
 
 import devopsdistilled.operp.client.abstracts.PaneModel;
 import devopsdistilled.operp.client.stock.panes.models.observers.TransferStockPaneModelObserver;
+import devopsdistilled.operp.server.data.entity.items.Item;
+import devopsdistilled.operp.server.data.entity.stock.Warehouse;
 
 public interface TransferStockPaneModel extends
 		PaneModel<TransferStockPaneModelObserver> {
+
+	void setFromWarehouse(Warehouse fromWarehouse);
+
+	void setItemToTransfer(Item itemtoTransfer);
+
+	void setToWarehouse(Warehouse toWarehouse);
+
+	Long getQuantity();
+
+	void setQuantity(Long quantity);
+
+	Warehouse getFromWarehouse();
+
+	Item getItemToTransfer();
+
+	Warehouse getToWarehouse();
 
 }
--- OpERP/src/main/java/devopsdistilled/operp/client/stock/panes/models/impl/TransferStockPaneModelImpl.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/stock/panes/models/impl/TransferStockPaneModelImpl.java
@@ -1,16 +1,81 @@
 package devopsdistilled.operp.client.stock.panes.models.impl;
 
+import java.util.List;
+
+import javax.inject.Inject;
+
 import devopsdistilled.operp.client.abstracts.AbstractPaneModel;
+import devopsdistilled.operp.client.stock.models.StockModel;
 import devopsdistilled.operp.client.stock.panes.models.TransferStockPaneModel;
 import devopsdistilled.operp.client.stock.panes.models.observers.TransferStockPaneModelObserver;
+import devopsdistilled.operp.server.data.entity.items.Item;
+import devopsdistilled.operp.server.data.entity.stock.Warehouse;
 
 public class TransferStockPaneModelImpl extends
 		AbstractPaneModel<TransferStockPaneModelObserver> implements
 		TransferStockPaneModel {
 
+	@Inject
+	private StockModel stockModel;
+
+	private List<Item> fromWarehouseItems;
+
+	private Warehouse fromWarehouse;
+
+	private Item itemToTransfer;
+
+	private Warehouse toWarehouse;
+
+	private Long quantity;
+
 	@Override
 	public String getTitle() {
 		return "Transfer Stock";
+	}
+
+	@Override
+	public Long getQuantity() {
+		return quantity;
+	}
+
+	@Override
+	public void setQuantity(Long quantity) {
+		this.quantity = quantity;
+	}
+
+	@Override
+	public Warehouse getFromWarehouse() {
+		return fromWarehouse;
+	}
+
+	@Override
+	public Item getItemToTransfer() {
+		return itemToTransfer;
+	}
+
+	@Override
+	public Warehouse getToWarehouse() {
+		return toWarehouse;
+	}
+
+	@Override
+	public void setFromWarehouse(Warehouse fromWarehouse) {
+		this.fromWarehouse = fromWarehouse;
+		fromWarehouseItems = stockModel.getService().getItemsInWarehouse(
+				fromWarehouse.getWarehouseId());
+		for (TransferStockPaneModelObserver observer : observers) {
+			observer.updateItems(fromWarehouseItems);
+		}
+	}
+
+	@Override
+	public void setItemToTransfer(Item itemtoTransfer) {
+		this.itemToTransfer = itemtoTransfer;
+	}
+
+	@Override
+	public void setToWarehouse(Warehouse toWarehouse) {
+		this.toWarehouse = toWarehouse;
 	}
 
 }
