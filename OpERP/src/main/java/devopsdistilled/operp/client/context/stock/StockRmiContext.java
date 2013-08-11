90175ef8f682739daa38d29087c91a9bc1fe7c61
Kapil Koju <kplx2.0@gmail.com>
2013-08-11 22:25:11 +0545

    RMI Proxy Bean for StockKeeperService
    OPEN - task 24: Create a stock module 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/24

--- OpERP/src/main/java/devopsdistilled/operp/client/context/stock/StockRmiContext.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/context/stock/StockRmiContext.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.remoting.rmi.RmiProxyFactoryBean;
 
+import devopsdistilled.operp.server.data.service.stock.StockKeeperService;
 import devopsdistilled.operp.server.data.service.stock.StockService;
 import devopsdistilled.operp.server.data.service.stock.WarehouseService;
 
@@ -23,6 +24,14 @@ public class StockRmiContext {
 		RmiProxyFactoryBean rmiProxy = new RmiProxyFactoryBean();
 		rmiProxy.setServiceUrl("rmi://127.0.1.1:1099/WarehouseService");
 		rmiProxy.setServiceInterface(WarehouseService.class);
+		return rmiProxy;
+	}
+
+	@Bean
+	public RmiProxyFactoryBean stockKeeperService() {
+		RmiProxyFactoryBean rmiProxy = new RmiProxyFactoryBean();
+		rmiProxy.setServiceUrl("rmi://127.0.1.1:1099/StockKeeperService");
+		rmiProxy.setServiceInterface(StockKeeperService.class);
 		return rmiProxy;
 	}
 
