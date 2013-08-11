1e238c05345f87a1d09c8415804bee6b58a33325
Kapil Koju <kplx2.0@gmail.com>
2013-08-11 22:09:35 +0545

    Impl save()
    OPEN - task 24: Create a stock module 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/24

--- OpERP/src/main/java/devopsdistilled/operp/server/data/service/stock/impl/StockKeeperServiceImpl.java
+++ OpERP/src/main/java/devopsdistilled/operp/server/data/service/stock/impl/StockKeeperServiceImpl.java
@@ -3,9 +3,12 @@ package devopsdistilled.operp.server.data.service.stock.impl;
 import javax.inject.Inject;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import devopsdistilled.operp.server.data.entity.stock.Stock;
 import devopsdistilled.operp.server.data.entity.stock.StockKeeper;
 import devopsdistilled.operp.server.data.repo.stock.StockKeeperRepository;
+import devopsdistilled.operp.server.data.repo.stock.StockRepository;
 import devopsdistilled.operp.server.data.service.impl.AbstractEntityService;
 import devopsdistilled.operp.server.data.service.stock.StockKeeperService;
 
@@ -19,6 +22,9 @@ public class StockKeeperServiceImpl extends
 	@Inject
 	private StockKeeperRepository repo;
 
+	@Inject
+	private StockRepository stockRepository;
+
 	@Override
 	protected StockKeeperRepository getRepo() {
 		return repo;
@@ -26,8 +32,25 @@ public class StockKeeperServiceImpl extends
 
 	@Override
 	protected StockKeeper findByEntityName(String entityName) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
+	@Override
+	@Transactional
+	public <S extends StockKeeper> S save(S stockKeeper) {
+		Stock stock = stockRepository.findByItemAndWarehouse(stockKeeper
+				.getStock().getItem(), stockKeeper.getStock().getWarehouse());
+
+		if (stock != null) {
+			stock.setQuantity(stock.getQuantity() + stockKeeper.getQuantity());
+		} else {
+			stock = stockKeeper.getStock();
+			stock.setQuantity(stockKeeper.getQuantity());
+		}
+		stock = stockRepository.save(stock);
+		stockKeeper.setStock(stock);
+
+		return super.save(stockKeeper);
+
+	}
 }
