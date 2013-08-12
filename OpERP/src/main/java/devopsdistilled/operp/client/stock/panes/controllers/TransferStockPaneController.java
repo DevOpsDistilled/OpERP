8093f4ec2d136c5769e69b68967b96a9bf38d337
Kapil Koju <kplx2.0@gmail.com>
2013-08-12 15:08:13 +0545

    Return StockKeepers from transfer()

--- OpERP/src/main/java/devopsdistilled/operp/client/stock/panes/controllers/TransferStockPaneController.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/stock/panes/controllers/TransferStockPaneController.java
@@ -1,14 +1,17 @@
 package devopsdistilled.operp.client.stock.panes.controllers;
 
+import java.util.List;
+
 import devopsdistilled.operp.client.abstracts.SubTaskPaneController;
 import devopsdistilled.operp.client.exceptions.EntityValidationException;
 import devopsdistilled.operp.client.stock.panes.models.TransferStockPaneModel;
+import devopsdistilled.operp.server.data.entity.stock.StockKeeper;
 
 public interface TransferStockPaneController extends SubTaskPaneController {
 
 	public void validate() throws EntityValidationException;
 
-	public void transfer();
+	public List<StockKeeper> transfer();
 
 	public TransferStockPaneModel getModel();
 
