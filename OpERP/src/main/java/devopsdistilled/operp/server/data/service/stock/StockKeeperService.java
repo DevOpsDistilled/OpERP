d4a6182264aea3e32f8ee2a60ba9d323b8afc1bf
Kapil Koju <kplx2.0@gmail.com>
2013-08-12 13:38:20 +0545

    Declare saveTransfer(src, dest)
    OPEN - task 24: Create a stock module 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/24

--- OpERP/src/main/java/devopsdistilled/operp/server/data/service/stock/StockKeeperService.java
+++ OpERP/src/main/java/devopsdistilled/operp/server/data/service/stock/StockKeeperService.java
@@ -1,8 +1,13 @@
 package devopsdistilled.operp.server.data.service.stock;
 
+import java.util.List;
+
 import devopsdistilled.operp.server.data.entity.stock.StockKeeper;
 import devopsdistilled.operp.server.data.service.EntityService;
 
 public interface StockKeeperService extends EntityService<StockKeeper, Long> {
+
+	List<StockKeeper> saveTransfer(StockKeeper srcStockKeeper,
+			StockKeeper destStockKeeper);
 
 }
