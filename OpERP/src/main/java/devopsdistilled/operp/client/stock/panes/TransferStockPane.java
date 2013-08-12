84099a42ad497f4e7380b0983820f2d3712738b1
Kapil Koju <kplx2.0@gmail.com>
2013-08-12 13:57:07 +0545

    Undo remove warehouse from toWarehouseCombo

--- OpERP/src/main/java/devopsdistilled/operp/client/stock/panes/TransferStockPane.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/stock/panes/TransferStockPane.java
@@ -162,10 +162,8 @@ public class TransferStockPane extends SubTaskPane implements
 			toWarehouseCombo.addItem(warehouse);
 
 			if (prevSelectedFrom != null)
-				if (prevSelectedFrom.compareTo(warehouse) == 0) {
+				if (prevSelectedFrom.compareTo(warehouse) == 0)
 					fromWarehouseCombo.setSelectedItem(warehouse);
-					toWarehouseCombo.removeItem(warehouse);
-				}
 
 			if (prevSelectedTo != null)
 				if (prevSelectedTo.compareTo(warehouse) == 0)
