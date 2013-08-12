224cd904cc20c1835a0f152d6549a1e340920c65
invincible <invinciblesharad@gmail.com>
2013-08-12 22:36:34 +0545

    Add methods for imageResizing and setting button size

--- OpERP/src/main/java/devopsdistilled/operp/client/abstracts/TaskPane.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/abstracts/TaskPane.java
@@ -1,8 +1,38 @@
 package devopsdistilled.operp.client.abstracts;
 
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
+
 public abstract class TaskPane extends ViewPane {
 
 	@Override
 	public abstract String toString();
+	
+	public  Icon SetSizeForImage(ImageIcon icon){
+		Image img=icon.getImage();
+		BufferedImage bi=new BufferedImage(70, 60, BufferedImage.TYPE_INT_ARGB);
+		Graphics g=bi.createGraphics();
+		g.drawImage(img,0,0,70,60, null);
+		Icon newIcon=new ImageIcon(bi);
+		return newIcon;
+		
+	}
+	
+	public JButton SetButtonSize(JButton btn){
+		btn.setMaximumSize(new Dimension(90, 90));
+		btn.setMinimumSize(new Dimension(90,90));
+		btn.setHorizontalTextPosition(SwingConstants.CENTER);
+		btn.setVerticalTextPosition(SwingConstants.BOTTOM);
+		btn.setFont(new Font("Arial", Font.PLAIN, 10));
+		return btn;
+	}
 
 }
