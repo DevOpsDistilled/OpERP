848ac2b271a37cdb1414cfbdfcf4ab905b23de27
Kapil Koju <kplx2.0@gmail.com>
2013-08-12 13:38:52 +0545

    Specify JoinColumn name
    OPEN - task 24: Create a stock module 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/24

--- OpERP/src/main/java/devopsdistilled/operp/server/data/entity/stock/StockKeeper.java
+++ OpERP/src/main/java/devopsdistilled/operp/server/data/entity/stock/StockKeeper.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Temporal;
@@ -32,6 +33,7 @@ public class StockKeeper extends Entiti<Long> {
 	private Long quantity;
 
 	@OneToOne
+	@JoinColumn(name = "transferStockKeeperId")
 	private StockKeeper transferStockKeeper;
 
 	public Long getStockKeeperId() {
