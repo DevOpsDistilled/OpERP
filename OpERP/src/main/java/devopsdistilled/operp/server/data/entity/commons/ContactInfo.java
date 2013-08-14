0e5e943aa8c2100dcc7a3a4c32a3f10ca3522942
Kapil Koju <kplx2.0@gmail.com>
2013-08-14 09:46:49 +0545

    Finalize Entity with Map phoneNumbers
    OPEN - task 80: Create common entities 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/80

--- OpERP/src/main/java/devopsdistilled/operp/server/data/entity/commons/ContactInfo.java
+++ OpERP/src/main/java/devopsdistilled/operp/server/data/entity/commons/ContactInfo.java
@@ -1,13 +1,22 @@
 package devopsdistilled.operp.server.data.entity.commons;
 
-import java.util.List;
+import java.util.Map;
 
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.MapKeyColumn;
+import javax.persistence.MapKeyEnumerated;
+import javax.persistence.OneToOne;
 
 import devopsdistilled.operp.server.data.entity.Entiti;
 
+@Entity
 public class ContactInfo extends Entiti<Long> {
 
 	private static final long serialVersionUID = 2729474004283929872L;
@@ -16,16 +25,53 @@ public class ContactInfo extends Entiti<Long> {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long contactId;
 
+	@OneToOne
 	private Address address;
 
 	private String email;
 
-	private List<Phone> phoneNumbers;
+	@ElementCollection
+	@CollectionTable(name = "PhoneNumber")
+	@MapKeyEnumerated(EnumType.STRING)
+	@MapKeyColumn(name = "phoneType")
+	@Column(name = "phoneNumber")
+	private Map<PhoneType, String> phoneNumbers;
+
+	public Long getContactId() {
+		return contactId;
+	}
+
+	public void setContactId(Long contactId) {
+		this.contactId = contactId;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Map<PhoneType, String> getPhoneNumbers() {
+		return phoneNumbers;
+	}
+
+	public void setPhoneNumbers(Map<PhoneType, String> phoneNumbers) {
+		this.phoneNumbers = phoneNumbers;
+	}
 
 	@Override
 	public Long id() {
-		// TODO Auto-generated method stub
-		return null;
+		return getContactId();
 	}
 
 	@Override
