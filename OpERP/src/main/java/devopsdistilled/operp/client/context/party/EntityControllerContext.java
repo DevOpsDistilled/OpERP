b3965d8ecbf672cfdaf7ff8a1287942ca5dc77f1
Kapil Koju <kplx2.0@gmail.com>
2013-08-15 18:17:14 +0545

    Bean VendorController
    OPEN - task 79: Create Party Module 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/79

--- OpERP/src/main/java/devopsdistilled/operp/client/context/party/EntityControllerContext.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/context/party/EntityControllerContext.java
@@ -1,8 +1,16 @@
 package devopsdistilled.operp.client.context.party;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import devopsdistilled.operp.client.party.controllers.VendorController;
+import devopsdistilled.operp.client.party.controllers.impl.VendorControllerImpl;
 
 @Configuration
 public class EntityControllerContext {
 
+	@Bean
+	public VendorController vendorController() {
+		return new VendorControllerImpl();
+	}
 }
