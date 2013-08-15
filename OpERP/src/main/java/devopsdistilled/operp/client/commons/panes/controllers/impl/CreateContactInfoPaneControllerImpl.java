e44530d5983fac5eea1cea8b5115346701e33c91
Kapil Koju <kplx2.0@gmail.com>
2013-08-15 21:44:02 +0545

    Add AddressPane
    OPEN - task 79: Create Party Module 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/79

--- OpERP/src/main/java/devopsdistilled/operp/client/commons/panes/controllers/impl/CreateContactInfoPaneControllerImpl.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/commons/panes/controllers/impl/CreateContactInfoPaneControllerImpl.java
@@ -1,8 +1,10 @@
 package devopsdistilled.operp.client.commons.panes.controllers.impl;
 
 import javax.inject.Inject;
+import javax.swing.JPanel;
 
 import devopsdistilled.operp.client.commons.panes.CreateContactInfoPane;
+import devopsdistilled.operp.client.commons.panes.controllers.CreateAddressPaneController;
 import devopsdistilled.operp.client.commons.panes.controllers.CreateContactInfoPaneController;
 import devopsdistilled.operp.client.commons.panes.models.CreateContactInfoPaneModel;
 import devopsdistilled.operp.client.exceptions.EntityValidationException;
@@ -16,6 +18,9 @@ public class CreateContactInfoPaneControllerImpl implements
 
 	@Inject
 	private CreateContactInfoPaneModel model;
+
+	@Inject
+	private CreateAddressPaneController createAddressPaneController;
 
 	@Override
 	public void validate(ContactInfo entity) throws EntityValidationException {
@@ -36,7 +41,16 @@ public class CreateContactInfoPaneControllerImpl implements
 
 	@Override
 	public void init() {
-		view.init();
+
+		createAddressPaneController.init();
+		
+		// Setting controller is required
+		// if Bean scope set to Prototype
+		createAddressPaneController.getView().setController(
+				createAddressPaneController);
+		view.setAddressPanel((JPanel) createAddressPaneController.getView()
+				.getPane());
+		// view.init();
 		model.registerObserver(view);
 	}
 
