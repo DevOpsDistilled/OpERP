040e5678c07860cdbd057ab654d29c7fa955cedb
Kapil Koju <kplx2.0@gmail.com>
2013-08-15 17:22:42 +0545

    init party-module context components
    OPEN - task 79: Create Party Module 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/79

---
+++ OpERP/src/main/java/devopsdistilled/operp/client/context/party/MvcControllerContext.java
@@ -0,0 +1,8 @@
+package devopsdistilled.operp.client.context.party;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MvcControllerContext {
+
+}
---
+++ OpERP/src/main/java/devopsdistilled/operp/client/context/party/PartyRmiContext.java
@@ -0,0 +1,10 @@
+package devopsdistilled.operp.client.context.party;
+
+import org.springframework.context.annotation.Configuration;
+
+import devopsdistilled.operp.client.context.RmiContext;
+
+@Configuration
+public class PartyRmiContext extends RmiContext {
+
+}
