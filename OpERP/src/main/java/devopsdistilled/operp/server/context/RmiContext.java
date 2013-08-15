753924475447dc8d48200c678bae0bc3ba0bde44
Kapil Koju <kplx2.0@gmail.com>
2013-08-15 17:41:27 +0545

    Import PartyRmiContext
    OPEN - task 79: Create Party Module 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/79

--- OpERP/src/main/java/devopsdistilled/operp/server/context/RmiContext.java
+++ OpERP/src/main/java/devopsdistilled/operp/server/context/RmiContext.java
@@ -5,10 +5,12 @@ import org.springframework.context.annotation.Import;
 
 import devopsdistilled.operp.server.context.commons.CommonRmiContext;
 import devopsdistilled.operp.server.context.items.ItemRmiContext;
+import devopsdistilled.operp.server.context.party.PartyRmiContext;
 import devopsdistilled.operp.server.context.stock.StockRmiContext;
 
 @Configuration
-@Import({ CommonRmiContext.class, ItemRmiContext.class, StockRmiContext.class })
+@Import({ CommonRmiContext.class, ItemRmiContext.class, StockRmiContext.class,
+		PartyRmiContext.class })
 public class RmiContext {
 
 }
