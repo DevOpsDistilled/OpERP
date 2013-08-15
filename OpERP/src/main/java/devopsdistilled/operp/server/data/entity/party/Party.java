f0a68c58f8002d20c76efff6c5b739882abf9e2a
Kapil Koju <kplx2.0@gmail.com>
2013-08-15 23:15:45 +0545

    Add ContactInfo as attribute.
    OPEN - task 79: Create Party Module 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/79

--- OpERP/src/main/java/devopsdistilled/operp/server/data/entity/party/Party.java
+++ OpERP/src/main/java/devopsdistilled/operp/server/data/entity/party/Party.java
@@ -6,10 +6,12 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.OneToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 import devopsdistilled.operp.server.data.entity.Entiti;
+import devopsdistilled.operp.server.data.entity.commons.ContactInfo;
 
 @MappedSuperclass
 public abstract class Party extends Entiti<Long> {
@@ -23,6 +25,9 @@ public abstract class Party extends Entiti<Long> {
 	private String partyName;
 
 	private String panVat;
+
+	@OneToOne
+	private ContactInfo contactInfo;
 
 	@Temporal(TemporalType.DATE)
 	private Date registerdDate;
@@ -49,6 +54,14 @@ public abstract class Party extends Entiti<Long> {
 
 	public void setPanVat(String panVat) {
 		this.panVat = panVat;
+	}
+
+	public ContactInfo getContactInfo() {
+		return contactInfo;
+	}
+
+	public void setContactInfo(ContactInfo contactInfo) {
+		this.contactInfo = contactInfo;
 	}
 
 	public Date getRegisterdDate() {
