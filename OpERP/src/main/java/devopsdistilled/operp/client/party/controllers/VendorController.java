5082a4815d2c34e1ab8b5a354bdba1d51cbb694e
Kapil Koju <kplx2.0@gmail.com>
2013-08-15 18:15:56 +0545

    Add VendorController
    OPEN - task 79: Create Party Module 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/79

---
+++ OpERP/src/main/java/devopsdistilled/operp/client/party/controllers/VendorController.java
@@ -0,0 +1,8 @@
+package devopsdistilled.operp.client.party.controllers;
+
+import devopsdistilled.operp.client.abstracts.EntityController;
+import devopsdistilled.operp.server.data.entity.party.Vendor;
+
+public interface VendorController extends EntityController<Vendor> {
+
+}
