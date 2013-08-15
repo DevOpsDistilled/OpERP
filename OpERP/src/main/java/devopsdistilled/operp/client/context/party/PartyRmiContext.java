a56ace60ad06974090f14bc246f485b84b9f04e5
Kapil Koju <kplx2.0@gmail.com>
2013-08-15 17:42:56 +0545

    Proxy Bean VendorService
    OPEN - task 79: Create Party Module 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/79

--- OpERP/src/main/java/devopsdistilled/operp/client/context/party/PartyRmiContext.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/context/party/PartyRmiContext.java
@@ -1,10 +1,20 @@
 package devopsdistilled.operp.client.context.party;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.remoting.rmi.RmiProxyFactoryBean;
 
 import devopsdistilled.operp.client.context.RmiContext;
+import devopsdistilled.operp.server.data.service.party.VendorService;
 
 @Configuration
 public class PartyRmiContext extends RmiContext {
 
+	@Bean
+	public RmiProxyFactoryBean vendorService() {
+		RmiProxyFactoryBean rmiProxy = new RmiProxyFactoryBean();
+		rmiProxy.setServiceUrl(rmiUrl + "/VendorService");
+		rmiProxy.setServiceInterface(VendorService.class);
+		return rmiProxy;
+	}
 }
