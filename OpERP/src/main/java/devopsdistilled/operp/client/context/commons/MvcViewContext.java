18bb2fc00d5ef9331f0358710c186a48d8765780
Kapil Koju <kplx2.0@gmail.com>
2013-08-15 17:29:12 +0545

    Bean Create Address/ContactInfo MVC components
    OPEN - task 79: Create Party Module 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/79

--- OpERP/src/main/java/devopsdistilled/operp/client/context/commons/MvcViewContext.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/context/commons/MvcViewContext.java
@@ -1,8 +1,22 @@
 package devopsdistilled.operp.client.context.commons;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import devopsdistilled.operp.client.commons.panes.CreateAddressPane;
+import devopsdistilled.operp.client.commons.panes.CreateContactInfoPane;
 
 @Configuration
 public class MvcViewContext {
+
+	@Bean
+	public CreateAddressPane createAddressPane() {
+		return new CreateAddressPane();
+	}
+
+	@Bean
+	public CreateContactInfoPane createContactInfoPane() {
+		return new CreateContactInfoPane();
+	}
 
 }
