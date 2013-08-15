a9ff0f398f02c403cad71fb6e3c26815ab787ef3
Kapil Koju <kplx2.0@gmail.com>
2013-08-15 17:34:11 +0545

    Add Vendor EntityModel components
    OPEN - task 80: Create common entities 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/80

---
+++ OpERP/src/main/java/devopsdistilled/operp/client/party/models/observers/VendorModelObserver.java
@@ -0,0 +1,7 @@
+package devopsdistilled.operp.client.party.models.observers;
+
+import devopsdistilled.operp.client.abstracts.EntityModelObserver;
+
+public interface VendorModelObserver extends EntityModelObserver {
+
+}
