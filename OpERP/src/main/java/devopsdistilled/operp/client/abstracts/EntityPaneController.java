2034d6551d29191acde494b4dba931501e7ce170
Kapil Koju <kplx2.0@gmail.com>
2013-08-16 15:48:59 +0545

    Correct Argument Type

--- OpERP/src/main/java/devopsdistilled/operp/client/abstracts/EntityPaneController.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/abstracts/EntityPaneController.java
@@ -3,7 +3,7 @@ package devopsdistilled.operp.client.abstracts;
 import devopsdistilled.operp.client.exceptions.EntityValidationException;
 import devopsdistilled.operp.server.data.entity.Entiti;
 
-public interface EntityPaneController<E extends Entiti<?>, M extends EntityModel<E, ?, ?>>
+public interface EntityPaneController<E extends Entiti<?>, M extends EntityPaneModel<E, ?>>
 		extends SubTaskPaneController {
 
 	public void validate() throws EntityValidationException;
