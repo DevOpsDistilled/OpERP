98c5827e0d82440cc5f41b9e69f51b521086a213
Kapil Koju <kplx2.0@gmail.com>
2013-08-17 16:33:56 +0545

    Extend AbstractEntityPaneModel

--- OpERP/src/main/java/devopsdistilled/operp/client/commons/panes/models/impl/CreateAddressPaneModelImpl.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/commons/panes/models/impl/CreateAddressPaneModelImpl.java
@@ -1,24 +1,17 @@
 package devopsdistilled.operp.client.commons.panes.models.impl;
 
-import devopsdistilled.operp.client.abstracts.AbstractPaneModel;
+import devopsdistilled.operp.client.abstracts.AbstractEntityPaneModel;
 import devopsdistilled.operp.client.commons.panes.models.CreateAddressPaneModel;
 import devopsdistilled.operp.client.commons.panes.models.observers.CreateAddressPaneModelObserver;
 import devopsdistilled.operp.server.data.entity.commons.Address;
 
 public class CreateAddressPaneModelImpl extends
-		AbstractPaneModel<CreateAddressPaneModelObserver> implements
-		CreateAddressPaneModel {
-
-	private final Address address = new Address();
+		AbstractEntityPaneModel<Address, CreateAddressPaneModelObserver>
+		implements CreateAddressPaneModel {
 
 	@Override
 	public String getTitle() {
 		return "Create Address";
-	}
-
-	@Override
-	public Address getAddress() {
-		return address;
 	}
 
 }
