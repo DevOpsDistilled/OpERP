9530cf7df938c6b24a590e24b2a1aa011abaa959
Kapil Koju <kplx2.0@gmail.com>
2013-08-18 21:07:46 +0545

    Add utility methods to give reasonable column names in Vendor List
    OPEN - task 79: Create Party Module 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/79

--- OpERP/src/main/java/devopsdistilled/operp/server/data/entity/party/Vendor.java
+++ OpERP/src/main/java/devopsdistilled/operp/server/data/entity/party/Vendor.java
@@ -7,4 +7,12 @@ public class Vendor extends Party {
 
 	private static final long serialVersionUID = 1438740246885453097L;
 
+	public Long getVendorId() {
+		return getPartyId();
+	}
+
+	public String getVendorName() {
+		return getPartyName();
+	}
+
 }
