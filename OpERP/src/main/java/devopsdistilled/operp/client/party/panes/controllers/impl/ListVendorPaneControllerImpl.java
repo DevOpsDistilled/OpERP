3df9133e71c75e11cbfd1abe61d51a9122708afd
Kapil Koju <kplx2.0@gmail.com>
2013-08-18 21:06:11 +0545

    Show list of vendors
    OPEN - task 79: Create Party Module 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/79

--- OpERP/src/main/java/devopsdistilled/operp/client/party/panes/controllers/impl/ListVendorPaneControllerImpl.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/party/panes/controllers/impl/ListVendorPaneControllerImpl.java
@@ -2,6 +2,7 @@ package devopsdistilled.operp.client.party.panes.controllers.impl;
 
 import javax.inject.Inject;
 
+import devopsdistilled.operp.client.party.models.VendorModel;
 import devopsdistilled.operp.client.party.panes.ListVendorPane;
 import devopsdistilled.operp.client.party.panes.controllers.ListVendorPaneController;
 import devopsdistilled.operp.client.party.panes.models.ListVendorPaneModel;
@@ -14,10 +15,14 @@ public class ListVendorPaneControllerImpl implements ListVendorPaneController {
 	@Inject
 	private ListVendorPaneModel model;
 
+	@Inject
+	private VendorModel vendorModel;
+
 	@Override
 	public void init() {
 		view.init();
 		model.registerObserver(view);
+		vendorModel.registerObserver(view);
 	}
 
 }
