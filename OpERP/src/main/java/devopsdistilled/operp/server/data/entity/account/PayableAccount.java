97bb2c0258ca570b199060c1f21b9233f295e83e
Kapil Koju <kplx2.0@gmail.com>
2013-08-19 22:09:13 +0545

    Reference to transactions
    OPEN - task 79: Create Party Module 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/79

--- OpERP/src/main/java/devopsdistilled/operp/server/data/entity/account/PayableAccount.java
+++ OpERP/src/main/java/devopsdistilled/operp/server/data/entity/account/PayableAccount.java
@@ -1,7 +1,10 @@
 package devopsdistilled.operp.server.data.entity.account;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.Inheritance;
+import javax.persistence.OneToMany;
 
 @Entity
 @Inheritance
@@ -9,4 +12,14 @@ public class PayableAccount extends Account {
 
 	private static final long serialVersionUID = 3505195542194191807L;
 
+	@OneToMany(mappedBy = "account")
+	private List<PaidTransaction> transactions;
+
+	public List<PaidTransaction> getTransactions() {
+		return transactions;
+	}
+
+	public void setTransactions(List<PaidTransaction> transactions) {
+		this.transactions = transactions;
+	}
 }
--- OpERP/src/main/java/devopsdistilled/operp/server/data/entity/account/ReceivableAccount.java
+++ OpERP/src/main/java/devopsdistilled/operp/server/data/entity/account/ReceivableAccount.java
@@ -1,10 +1,24 @@
 package devopsdistilled.operp.server.data.entity.account;
 
+import java.util.List;
+
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 
 @Entity
 public class ReceivableAccount extends Account {
 
 	private static final long serialVersionUID = 6812190478232748288L;
+
+	@OneToMany(mappedBy = "account")
+	private List<ReceivedTransaction> transactions;
+
+	public List<ReceivedTransaction> getTransactions() {
+		return transactions;
+	}
+
+	public void setTransactions(List<ReceivedTransaction> transactions) {
+		this.transactions = transactions;
+	}
 
 }
