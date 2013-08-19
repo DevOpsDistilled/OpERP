32ff11cb4e5b34588c9923018f218f6b3d816b20
Kapil Koju <kplx2.0@gmail.com>
2013-08-19 15:32:22 +0545

    Bean CustomerModel
    OPEN - task 79: Create Party Module 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/79

--- OpERP/src/main/java/devopsdistilled/operp/client/context/party/EntityModelContext.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/context/party/EntityModelContext.java
@@ -3,7 +3,9 @@ package devopsdistilled.operp.client.context.party;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import devopsdistilled.operp.client.party.models.CustomerModel;
 import devopsdistilled.operp.client.party.models.VendorModel;
+import devopsdistilled.operp.client.party.models.impl.CustomerModelImpl;
 import devopsdistilled.operp.client.party.models.impl.VendorModelImpl;
 
 @Configuration
@@ -12,5 +14,10 @@ public class EntityModelContext {
 	@Bean
 	public VendorModel vendorModel() {
 		return new VendorModelImpl();
+	}
+
+	@Bean
+	public CustomerModel customerModel() {
+		return new CustomerModelImpl();
 	}
 }
