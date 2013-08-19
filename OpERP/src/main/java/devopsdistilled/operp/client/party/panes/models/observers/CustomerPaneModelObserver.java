c435745d50f9b5af5d9089a18fcf384341b15d43
Kapil Koju <kplx2.0@gmail.com>
2013-08-19 15:51:35 +0545

    Add CustomerPane Model & Observer
    OPEN - task 79: Create Party Module 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/79

---
+++ OpERP/src/main/java/devopsdistilled/operp/client/party/panes/models/observers/CustomerPaneModelObserver.java
@@ -0,0 +1,9 @@
+package devopsdistilled.operp.client.party.panes.models.observers;
+
+import devopsdistilled.operp.client.abstracts.EntityPaneModelObserver;
+import devopsdistilled.operp.server.data.entity.party.Customer;
+
+public interface CustomerPaneModelObserver extends
+		EntityPaneModelObserver<Customer> {
+
+}
