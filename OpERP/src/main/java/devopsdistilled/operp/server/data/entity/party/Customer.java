b122ef768d5298638739c5925abc4be5f6a8b895
Kapil Koju <kplx2.0@gmail.com>
2013-08-20 09:19:20 +0545

    Make reference to Business
    OPEN - task 86: Create Business(Sales-Purchases) Module 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/86

--- OpERP/src/main/java/devopsdistilled/operp/server/data/entity/party/Customer.java
+++ OpERP/src/main/java/devopsdistilled/operp/server/data/entity/party/Customer.java
@@ -1,11 +1,15 @@
 package devopsdistilled.operp.server.data.entity.party;
 
+import java.util.List;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 import devopsdistilled.operp.server.data.entity.account.Receivable;
 import devopsdistilled.operp.server.data.entity.account.ReceivableAccount;
+import devopsdistilled.operp.server.data.entity.business.Sale;
 
 @Entity
 public class Customer extends Party implements Receivable {
@@ -15,12 +19,23 @@ public class Customer extends Party implements Receivable {
 	@OneToOne(cascade = CascadeType.ALL)
 	private ReceivableAccount account;
 
+	@OneToMany(mappedBy = "party")
+	private List<Sale> sales;
+
 	public ReceivableAccount getAccount() {
 		return account;
 	}
 
 	public void setAccount(ReceivableAccount account) {
 		this.account = account;
+	}
+
+	public List<Sale> getSales() {
+		return sales;
+	}
+
+	public void setSales(List<Sale> sales) {
+		this.sales = sales;
 	}
 
 }
--- OpERP/src/main/java/devopsdistilled/operp/server/data/entity/party/Vendor.java
+++ OpERP/src/main/java/devopsdistilled/operp/server/data/entity/party/Vendor.java
@@ -1,11 +1,15 @@
 package devopsdistilled.operp.server.data.entity.party;
 
+import java.util.List;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 import devopsdistilled.operp.server.data.entity.account.Payable;
 import devopsdistilled.operp.server.data.entity.account.PayableAccount;
+import devopsdistilled.operp.server.data.entity.business.Purchase;
 
 @Entity
 public class Vendor extends Party implements Payable {
@@ -15,12 +19,23 @@ public class Vendor extends Party implements Payable {
 	@OneToOne(cascade = CascadeType.ALL)
 	private PayableAccount account;
 
+	@OneToMany(mappedBy = "party")
+	private List<Purchase> purchases;
+
 	public PayableAccount getAccount() {
 		return account;
 	}
 
 	public void setAccount(PayableAccount account) {
 		this.account = account;
+	}
+
+	public List<Purchase> getPurchases() {
+		return purchases;
+	}
+
+	public void setPurchases(List<Purchase> purchases) {
+		this.purchases = purchases;
 	}
 
 }
