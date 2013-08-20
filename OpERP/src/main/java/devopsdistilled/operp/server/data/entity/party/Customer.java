ab6d3ea43e694e545dd2fbcb44542792b742881c
Kapil Koju <kplx2.0@gmail.com>
2013-08-20 21:42:43 +0545

    Use Generics and Inheritance with entity attributes

--- OpERP/src/main/java/devopsdistilled/operp/server/data/entity/party/Customer.java
+++ OpERP/src/main/java/devopsdistilled/operp/server/data/entity/party/Customer.java
@@ -1,26 +1,13 @@
 package devopsdistilled.operp.server.data.entity.party;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.OneToOne;
 
 import devopsdistilled.operp.server.data.entity.account.Receivable;
 import devopsdistilled.operp.server.data.entity.account.ReceivableAccount;
 
 @Entity
-public class Customer extends Party implements Receivable {
+public class Customer extends Party<ReceivableAccount> implements Receivable {
 
 	private static final long serialVersionUID = 1504040341006393665L;
-
-	@OneToOne(cascade = CascadeType.ALL)
-	private ReceivableAccount account;
-
-	public ReceivableAccount getAccount() {
-		return account;
-	}
-
-	public void setAccount(ReceivableAccount account) {
-		this.account = account;
-	}
 
 }
