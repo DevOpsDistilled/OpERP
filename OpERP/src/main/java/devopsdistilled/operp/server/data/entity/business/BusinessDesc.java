c6c5ef114f0bc167318d6e6e2f3315c39eb01c4c
Kapil Koju <kplx2.0@gmail.com>
2013-08-20 15:07:16 +0545

    Validate BusinessDesc and DescRow relationship
    OPEN - task 86: Create Business(Sales-Purchases) Module 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/86

--- OpERP/src/main/java/devopsdistilled/operp/server/data/entity/business/BusinessDesc.java
+++ OpERP/src/main/java/devopsdistilled/operp/server/data/entity/business/BusinessDesc.java
@@ -1,9 +1,13 @@
 package devopsdistilled.operp.server.data.entity.business;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.OneToMany;
 
 import devopsdistilled.operp.server.data.entity.Entiti;
 
@@ -16,7 +20,16 @@ public abstract class BusinessDesc extends Entiti<Long> {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long descId;
 
+	@OneToMany
+	private final List<BusinessDescRow> descRows = new LinkedList<>();
+
+	public List<BusinessDescRow> getDescRows() {
+		return descRows;
+	}
+
 	private Double discountAmount;
+
+	private Double TotalAmount;
 
 	public Long getDescId() {
 		return descId;
@@ -32,6 +45,14 @@ public abstract class BusinessDesc extends Entiti<Long> {
 
 	public void setDiscountAmount(Double discountAmount) {
 		this.discountAmount = discountAmount;
+	}
+
+	public Double getTotalAmount() {
+		return TotalAmount;
+	}
+
+	public void setTotalAmount(Double totalAmount) {
+		TotalAmount = totalAmount;
 	}
 
 	@Override
--- OpERP/src/main/java/devopsdistilled/operp/server/data/entity/business/BusinessDescRow.java
+++ OpERP/src/main/java/devopsdistilled/operp/server/data/entity/business/BusinessDescRow.java
@@ -1,13 +1,60 @@
 package devopsdistilled.operp.server.data.entity.business;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
 
 import devopsdistilled.operp.server.data.entity.Entiti;
+import devopsdistilled.operp.server.data.entity.items.Item;
 
 @Entity
 public class BusinessDescRow extends Entiti<Long> {
 
-	private static final long serialVersionUID = -7388673538429668239L;
+	private static final long serialVersionUID = 9140270277994676490L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long descRowId;
+
+	@OneToOne
+	private Item item;
+	private Double rate;
+	private Long quantity;
+	private Double amount;
+
+	public Item getItem() {
+		return item;
+	}
+
+	public void setItem(Item item) {
+		this.item = item;
+	}
+
+	public Double getRate() {
+		return rate;
+	}
+
+	public void setRate(Double rate) {
+		this.rate = rate;
+	}
+
+	public Long getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Long quantity) {
+		this.quantity = quantity;
+	}
+
+	public Double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
 
 	@Override
 	public Long id() {
@@ -20,5 +67,4 @@ public class BusinessDescRow extends Entiti<Long> {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
 }
