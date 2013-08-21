949cc76e547f597ffc323f2799f7a2a0482d0990
Kapil Koju <kplx2.0@gmail.com>
2013-08-21 14:52:16 +0545

    Remove IdClass definitions

--- OpERP/src/main/java/devopsdistilled/operp/server/data/entity/business/PurchaseDescRow.java
+++ OpERP/src/main/java/devopsdistilled/operp/server/data/entity/business/PurchaseDescRow.java
@@ -1,10 +1,8 @@
 package devopsdistilled.operp.server.data.entity.business;
 
 import javax.persistence.Entity;
-import javax.persistence.IdClass;
 
 @Entity
-@IdClass(PurchaseDescRowId.class)
 public class PurchaseDescRow extends BusinessDescRow<PurchaseDesc> {
 
 	private static final long serialVersionUID = -7576094559879111898L;
--- OpERP/src/main/java/devopsdistilled/operp/server/data/entity/business/SaleDescRow.java
+++ OpERP/src/main/java/devopsdistilled/operp/server/data/entity/business/SaleDescRow.java
@@ -1,10 +1,8 @@
 package devopsdistilled.operp.server.data.entity.business;
 
 import javax.persistence.Entity;
-import javax.persistence.IdClass;
 
 @Entity
-@IdClass(SaleDescRowId.class)
 public class SaleDescRow extends BusinessDescRow<SaleDesc> {
 
 	private static final long serialVersionUID = 8479701082376399518L;
