8cbcb8ea071e6617d3efba8b5761f6c2194bcef8
Kapil Koju <kplx2.0@gmail.com>
2013-08-22 00:09:46 +0545

    Create basic SaleDescPane UI
    OPEN - task 86: Create Business(Sales-Purchases) Module 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/86

--- OpERP/src/main/java/devopsdistilled/operp/client/business/sales/panes/SaleDescPane.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/business/sales/panes/SaleDescPane.java
@@ -1,8 +1,12 @@
 package devopsdistilled.operp.client.business.sales.panes;
 
 import javax.swing.JComponent;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
 
+import net.miginfocom.swing.MigLayout;
 import devopsdistilled.operp.client.abstracts.EntityOperation;
 import devopsdistilled.operp.client.abstracts.EntityPane;
 import devopsdistilled.operp.client.business.sales.controllers.SaleDescController;
@@ -15,9 +19,34 @@ public class SaleDescPane extends
 		implements SaleDescPaneModelObserver {
 
 	private final JPanel pane;
+	private final JTextField discountField;
+	private final JTextField amountField;
 
 	public SaleDescPane() {
 		pane = new JPanel();
+		pane.setLayout(new MigLayout("", "[][grow]", "[][][]"));
+		
+		JPanel rowDescPanel = new JPanel();
+		pane.add(rowDescPanel, "cell 0 0,grow");
+		rowDescPanel.setLayout(new MigLayout("", "[]", "[]"));
+		
+		JScrollPane scrollPane = new JScrollPane();
+		pane.add(scrollPane, "cell 1 0,grow");
+		
+		JLabel lblDiscount = new JLabel("Discount");
+		pane.add(lblDiscount, "flowx,cell 1 1,alignx right");
+		
+		JLabel lblAmount = new JLabel("Amount");
+		pane.add(lblAmount, "flowx,cell 1 2,alignx right");
+		
+		discountField = new JTextField();
+		pane.add(discountField, "cell 1 1,alignx right");
+		discountField.setColumns(10);
+		
+		amountField = new JTextField();
+		amountField.setEditable(false);
+		pane.add(amountField, "cell 1 2,alignx right");
+		amountField.setColumns(10);
 	}
 
 	@Override
