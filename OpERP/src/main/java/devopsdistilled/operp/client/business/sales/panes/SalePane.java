7324375779ebdfa0e210aac91775625e41aa5fa3
Kapil Koju <kplx2.0@gmail.com>
2013-08-21 11:57:49 +0545

    Make implement PaneModelObserver
    OPEN - task 86: Create Business(Sales-Purchases) Module 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/86

--- OpERP/src/main/java/devopsdistilled/operp/client/business/sales/panes/SalePane.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/business/sales/panes/SalePane.java
@@ -4,13 +4,16 @@ import javax.inject.Inject;
 import javax.swing.JComponent;
 import javax.swing.JPanel;
 
+import devopsdistilled.operp.client.abstracts.EntityOperation;
 import devopsdistilled.operp.client.abstracts.EntityPane;
 import devopsdistilled.operp.client.business.sales.controllers.SaleController;
 import devopsdistilled.operp.client.business.sales.panes.controllers.SalePaneController;
+import devopsdistilled.operp.client.business.sales.panes.models.observers.SalePaneModelObserver;
 import devopsdistilled.operp.server.data.entity.business.Sale;
 
 public class SalePane extends
-		EntityPane<Sale, SaleController, SalePaneController> {
+		EntityPane<Sale, SaleController, SalePaneController> implements
+		SalePaneModelObserver {
 
 	@Inject
 	private SaleController saleController;
@@ -35,6 +38,12 @@ public class SalePane extends
 	@Override
 	public JComponent getPane() {
 		return pane;
+	}
+
+	@Override
+	public void updateEntity(Sale sale, EntityOperation entityOperation) {
+		// TODO Auto-generated method stub
+
 	}
 
 }
