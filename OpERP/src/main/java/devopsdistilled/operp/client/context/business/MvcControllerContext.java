095e14dbc83a84ce9e8aa674338a362ee81582b3
Kapil Koju <kplx2.0@gmail.com>
2013-08-21 12:03:12 +0545

    Bean SalePane Model & Controller
    OPEN - task 86: Create Business(Sales-Purchases) Module 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/86

--- OpERP/src/main/java/devopsdistilled/operp/client/context/business/MvcControllerContext.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/context/business/MvcControllerContext.java
@@ -1,9 +1,16 @@
 package devopsdistilled.operp.client.context.business;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import devopsdistilled.operp.client.business.sales.panes.controllers.SalePaneController;
+import devopsdistilled.operp.client.business.sales.panes.controllers.impl.SalePaneControllerImpl;
 
 @Configuration
 public class MvcControllerContext {
 
-	
+	@Bean
+	public SalePaneController salePaneController() {
+		return new SalePaneControllerImpl();
+	}
 }
--- OpERP/src/main/java/devopsdistilled/operp/client/context/business/MvcModelContext.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/context/business/MvcModelContext.java
@@ -1,9 +1,16 @@
 package devopsdistilled.operp.client.context.business;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import devopsdistilled.operp.client.business.sales.panes.models.SalePaneModel;
+import devopsdistilled.operp.client.business.sales.panes.models.impl.SalePaneModelImpl;
 
 @Configuration
 public class MvcModelContext {
 
-	
+	@Bean
+	public SalePaneModel salePaneModel() {
+		return new SalePaneModelImpl();
+	}
 }
