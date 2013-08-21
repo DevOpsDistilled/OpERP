c06f80c6401fcee41c6585a8e2547d6079658de9
Kapil Koju <kplx2.0@gmail.com>
2013-08-21 09:25:33 +0545

    Add "New Sale" button

--- OpERP/src/main/java/devopsdistilled/operp/client/sales/SalesMgmtPane.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/sales/SalesMgmtPane.java
@@ -1,9 +1,15 @@
 package devopsdistilled.operp.client.sales;
 
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import net.miginfocom.swing.MigLayout;
 import devopsdistilled.operp.client.abstracts.TaskPane;
 
 public class SalesMgmtPane extends TaskPane {
@@ -13,10 +19,24 @@ public class SalesMgmtPane extends TaskPane {
 		return new String("Sales Management");
 	}
 
+	/**
+	 * @wbp.parser.entryPoint
+	 */
 	@Override
 	public JComponent getPane() {
 		JPanel pane = new JPanel();
-		pane.add(new JLabel("Sales Management"));
+		pane.setLayout(new MigLayout("", "[136px,grow]", "[15px][][]"));
+		pane.add(new JLabel("Sales Management"),
+				"cell 0 0 2097051 1,alignx center,aligny top");
+
+		JButton btnNewSale = new JButton("New Sale");
+		btnNewSale.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewSale.setFont(new Font("DejaVu Sans", Font.PLAIN, 12));
+		pane.add(btnNewSale, "cell 0 2");
 		return pane;
 	}
 
