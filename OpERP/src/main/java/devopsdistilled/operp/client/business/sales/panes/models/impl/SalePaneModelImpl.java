bef378ad3d1df307cffc368ba74d3d81643cb3f0
Kapil Koju <kplx2.0@gmail.com>
2013-08-21 12:02:36 +0545

    Make implement PaneModel
    OPEN - task 86: Create Business(Sales-Purchases) Module 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/86

--- OpERP/src/main/java/devopsdistilled/operp/client/business/sales/panes/models/impl/SalePaneModelImpl.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/business/sales/panes/models/impl/SalePaneModelImpl.java
@@ -1,11 +1,13 @@
 package devopsdistilled.operp.client.business.sales.panes.models.impl;
 
 import devopsdistilled.operp.client.abstracts.AbstractEntityPaneModel;
+import devopsdistilled.operp.client.business.sales.panes.models.SalePaneModel;
 import devopsdistilled.operp.client.business.sales.panes.models.observers.SalePaneModelObserver;
 import devopsdistilled.operp.server.data.entity.business.Sale;
 
 public class SalePaneModelImpl extends
-		AbstractEntityPaneModel<Sale, SalePaneModelObserver> {
+		AbstractEntityPaneModel<Sale, SalePaneModelObserver> implements
+		SalePaneModel {
 
 	@Override
 	public String getTitle() {
