b72ede448f2aa42875f073b50db5bf4b3972b47d
Kapil Koju <kplx2.0@gmail.com>
2013-08-23 09:37:33 +0545

    Init & Set DescRow panel
    OPEN - task 86: Create Business(Sales-Purchases) Module 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/86

--- OpERP/src/main/java/devopsdistilled/operp/client/business/sales/panes/controllers/impl/SaleDescPaneControllerImpl.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/business/sales/panes/controllers/impl/SaleDescPaneControllerImpl.java
@@ -1,13 +1,16 @@
 package devopsdistilled.operp.client.business.sales.panes.controllers.impl;
 
 import javax.inject.Inject;
+import javax.swing.JPanel;
 
 import devopsdistilled.operp.client.abstracts.EntityOperation;
 import devopsdistilled.operp.client.business.sales.panes.SaleDescPane;
 import devopsdistilled.operp.client.business.sales.panes.controllers.SaleDescPaneController;
+import devopsdistilled.operp.client.business.sales.panes.controllers.SaleDescRowPaneController;
 import devopsdistilled.operp.client.business.sales.panes.models.SaleDescPaneModel;
 import devopsdistilled.operp.client.exceptions.EntityValidationException;
 import devopsdistilled.operp.server.data.entity.business.SaleDesc;
+import devopsdistilled.operp.server.data.entity.business.SaleDescRow;
 
 public class SaleDescPaneControllerImpl implements SaleDescPaneController {
 
@@ -16,6 +19,9 @@ public class SaleDescPaneControllerImpl implements SaleDescPaneController {
 
 	@Inject
 	private SaleDescPaneModel model;
+
+	@Inject
+	private SaleDescRowPaneController saleDescRowPaneController;
 
 	@Override
 	public void validate() throws EntityValidationException {
@@ -41,8 +47,14 @@ public class SaleDescPaneControllerImpl implements SaleDescPaneController {
 
 	@Override
 	public void init(SaleDesc saleDesc, EntityOperation entityOperation) {
-		// TODO Auto-generated method stub
-		
+		if (EntityOperation.Create == entityOperation) {
+			SaleDescRow saleDescRow = new SaleDescRow();
+			saleDesc.getDescRows().add(saleDescRow);
+			saleDescRowPaneController.init(saleDescRow, entityOperation);
+		}
+
+		view.setSaleDescRowpanel((JPanel) saleDescRowPaneController.getView()
+				.getPane());
 		view.setController(this);
 		view.resetComponents();
 
