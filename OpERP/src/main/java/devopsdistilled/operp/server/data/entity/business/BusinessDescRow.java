5d736b54f3feba06e50d2785bbeb28b7da4a436e
Kapil Koju <kplx2.0@gmail.com>
2013-08-23 12:14:37 +0545

    Add Getter/Setter for BusinessDesc
    OPEN - task 86: Create Business(Sales-Purchases) Module 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/86

--- OpERP/src/main/java/devopsdistilled/operp/server/data/entity/business/BusinessDescRow.java
+++ OpERP/src/main/java/devopsdistilled/operp/server/data/entity/business/BusinessDescRow.java
@@ -62,6 +62,14 @@ public abstract class BusinessDescRow<D extends BusinessDesc<?, ?>> extends
 		this.amount = amount;
 	}
 
+	public D getBusinessDesc() {
+		return businessDesc;
+	}
+
+	public void setBusinessDesc(D businessDesc) {
+		this.businessDesc = businessDesc;
+	}
+
 	@Override
 	public Long id() {
 		return null;
