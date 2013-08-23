84bddac4f8f32df939d83d409a574947a0afcdf1
Kapil Koju <kplx2.0@gmail.com>
2013-08-23 15:20:29 +0545

    Add temp SaleDescRow variable
    OPEN - task 86: Create Business(Sales-Purchases) Module 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/86

--- OpERP/src/main/java/devopsdistilled/operp/client/business/sales/panes/models/SaleDescPaneModel.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/business/sales/panes/models/SaleDescPaneModel.java
@@ -3,7 +3,12 @@ package devopsdistilled.operp.client.business.sales.panes.models;
 import devopsdistilled.operp.client.abstracts.EntityPaneModel;
 import devopsdistilled.operp.client.business.sales.panes.models.observers.SaleDescPaneModelObserver;
 import devopsdistilled.operp.server.data.entity.business.SaleDesc;
+import devopsdistilled.operp.server.data.entity.business.SaleDescRow;
 
 public interface SaleDescPaneModel extends
 		EntityPaneModel<SaleDesc, SaleDescPaneModelObserver> {
+
+	SaleDescRow getSaleDescRow();
+
+	void setSaleDescRow(SaleDescRow saleDescRow);
 }
