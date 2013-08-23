f96b6ddf617daa9f961706941fbf83320d5aa9b5
Kapil Koju <kplx2.0@gmail.com>
2013-08-23 18:23:54 +0545

    Declare Repositories for Business-module

---
+++ OpERP/src/main/java/devopsdistilled/operp/server/data/repo/business/SaleDescRepository.java
@@ -0,0 +1,9 @@
+package devopsdistilled.operp.server.data.repo.business;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import devopsdistilled.operp.server.data.entity.business.SaleDesc;
+
+public interface SaleDescRepository extends JpaRepository<SaleDesc, Long> {
+
+}
---
+++ OpERP/src/main/java/devopsdistilled/operp/server/data/repo/business/SaleDescRowRepository.java
@@ -0,0 +1,9 @@
+package devopsdistilled.operp.server.data.repo.business;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import devopsdistilled.operp.server.data.entity.business.SaleDescRow;
+
+public interface SaleDescRowRepository extends JpaRepository<SaleDescRow, Long> {
+
+}
