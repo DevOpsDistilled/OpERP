89e6ecff0469e585117ec71f5482f1ec6f735d4c
Kapil Koju <kplx2.0@gmail.com>
2013-08-23 08:47:19 +0545

    Set sales description pane
    OPEN - task 86: Create Business(Sales-Purchases) Module 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/86

--- OpERP/src/main/java/devopsdistilled/operp/client/business/sales/panes/controllers/impl/SalePaneControllerImpl.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/business/sales/panes/controllers/impl/SalePaneControllerImpl.java
@@ -1,13 +1,16 @@
 package devopsdistilled.operp.client.business.sales.panes.controllers.impl;
 
 import javax.inject.Inject;
+import javax.swing.JPanel;
 
 import devopsdistilled.operp.client.abstracts.EntityOperation;
 import devopsdistilled.operp.client.business.sales.panes.SalePane;
+import devopsdistilled.operp.client.business.sales.panes.controllers.SaleDescPaneController;
 import devopsdistilled.operp.client.business.sales.panes.controllers.SalePaneController;
 import devopsdistilled.operp.client.business.sales.panes.models.SalePaneModel;
 import devopsdistilled.operp.client.exceptions.EntityValidationException;
 import devopsdistilled.operp.server.data.entity.business.Sale;
+import devopsdistilled.operp.server.data.entity.business.SaleDesc;
 
 public class SalePaneControllerImpl implements SalePaneController {
 
@@ -16,6 +19,9 @@ public class SalePaneControllerImpl implements SalePaneController {
 
 	@Inject
 	private SalePane view;
+
+	@Inject
+	private SaleDescPaneController saleDescPaneController;
 
 	@Override
 	public void validate() throws EntityValidationException {
@@ -44,8 +50,9 @@ public class SalePaneControllerImpl implements SalePaneController {
 		// TODO Auto-generated method stub
 
 		if (EntityOperation.Create == entityOperation) {
-
-			// XXX
+			SaleDesc saleDesc = new SaleDesc();
+			sale.setBusinessDesc(saleDesc);
+			saleDescPaneController.init(saleDesc, EntityOperation.Create);
 
 		} else if (EntityOperation.Edit == entityOperation) {
 
@@ -56,6 +63,8 @@ public class SalePaneControllerImpl implements SalePaneController {
 			// XXX
 		}
 
+		view.setSaleDescpanel((JPanel) saleDescPaneController.getView()
+				.getPane());
 		view.setController(this);
 		view.resetComponents();
 
