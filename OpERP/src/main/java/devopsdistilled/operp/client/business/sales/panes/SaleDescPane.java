8d8eeb2d6a0c00fd43f033f875739c7bfc850272
Kapil Koju <kplx2.0@gmail.com>
2013-08-23 08:56:52 +0545

    Add Table for DescRows
    OPEN - task 86: Create Business(Sales-Purchases) Module 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/86

--- OpERP/src/main/java/devopsdistilled/operp/client/business/sales/panes/SaleDescPane.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/business/sales/panes/SaleDescPane.java
@@ -1,18 +1,27 @@
 package devopsdistilled.operp.client.business.sales.panes;
 
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
+import javax.swing.SwingUtilities;
 
 import net.miginfocom.swing.MigLayout;
 import devopsdistilled.operp.client.abstracts.EntityOperation;
 import devopsdistilled.operp.client.abstracts.EntityPane;
+import devopsdistilled.operp.client.abstracts.libs.BeanTableModel;
 import devopsdistilled.operp.client.business.sales.controllers.SaleDescController;
 import devopsdistilled.operp.client.business.sales.panes.controllers.SaleDescPaneController;
 import devopsdistilled.operp.client.business.sales.panes.models.observers.SaleDescPaneModelObserver;
+import devopsdistilled.operp.server.data.entity.business.BusinessDescRow;
 import devopsdistilled.operp.server.data.entity.business.SaleDesc;
+import devopsdistilled.operp.server.data.entity.business.SaleDescRow;
 
 public class SaleDescPane extends
 		EntityPane<SaleDesc, SaleDescController, SaleDescPaneController>
@@ -22,27 +31,48 @@ public class SaleDescPane extends
 	private final JTextField discountField;
 	private final JTextField amountField;
 
+	private final JTable table;
+	BeanTableModel<SaleDescRow> tableModel;
+
 	public SaleDescPane() {
 		pane = new JPanel();
 		pane.setLayout(new MigLayout("", "[][grow]", "[][][]"));
-		
+
 		JPanel rowDescPanel = new JPanel();
 		pane.add(rowDescPanel, "cell 0 0,grow");
 		rowDescPanel.setLayout(new MigLayout("", "[]", "[]"));
-		
-		JScrollPane scrollPane = new JScrollPane();
+
+		table = new JTable(tableModel);
+		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		table.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				if (SwingUtilities.isLeftMouseButton(e)
+						&& e.getClickCount() == 2
+						&& table.getSelectedRow() != -1) {
+					// SaleDescRow saleDescRow = tableModel.getRow(table
+					// .getSelectedRow());
+
+					// XXX
+				}
+			}
+		});
+
+		final JScrollPane scrollPane = new JScrollPane(table,
+				JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+				JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 		pane.add(scrollPane, "cell 1 0,grow");
-		
+
 		JLabel lblDiscount = new JLabel("Discount");
 		pane.add(lblDiscount, "flowx,cell 1 1,alignx right");
-		
+
 		JLabel lblAmount = new JLabel("Amount");
 		pane.add(lblAmount, "flowx,cell 1 2,alignx right");
-		
+
 		discountField = new JTextField();
 		pane.add(discountField, "cell 1 1,alignx right");
 		discountField.setColumns(10);
-		
+
 		amountField = new JTextField();
 		amountField.setEditable(false);
 		pane.add(amountField, "cell 1 2,alignx right");
@@ -57,7 +87,6 @@ public class SaleDescPane extends
 
 	@Override
 	public SaleDescController getEntityController() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
@@ -67,9 +96,17 @@ public class SaleDescPane extends
 	}
 
 	@Override
-	public void updateEntity(SaleDesc entity, EntityOperation entityOperation) {
-		// TODO Auto-generated method stub
+	public void updateEntity(SaleDesc saleDesc, EntityOperation entityOperation) {
+		if (EntityOperation.Create == entityOperation) {
+			tableModel = null;
+			tableModel = new BeanTableModel<>(SaleDescRow.class,
+					BusinessDescRow.class, saleDesc.getDescRows());
 
+			for (int i = 0; i < table.getColumnCount(); i++) {
+				tableModel.setColumnEditable(i, false);
+			}
+			tableModel.setModelEditable(false);
+			table.setModel(tableModel);
+		}
 	}
-
 }
