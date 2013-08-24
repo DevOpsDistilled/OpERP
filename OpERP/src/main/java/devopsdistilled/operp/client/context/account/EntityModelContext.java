fddece344060c967ce73ab56fd7e072ab3aae475
Kapil Koju <kplx2.0@gmail.com>
2013-08-24 23:32:31 +0545

    Bean EntityModel for ReceivedTransaction

--- OpERP/src/main/java/devopsdistilled/operp/client/context/account/EntityModelContext.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/context/account/EntityModelContext.java
@@ -5,8 +5,10 @@ import org.springframework.context.annotation.Configuration;
 
 import devopsdistilled.operp.client.account.models.PayableAccountModel;
 import devopsdistilled.operp.client.account.models.ReceivableAccountModel;
+import devopsdistilled.operp.client.account.models.ReceivedTransactionModel;
 import devopsdistilled.operp.client.account.models.impl.PayableAccountModelImpl;
 import devopsdistilled.operp.client.account.models.impl.ReceivableAccountModelImpl;
+import devopsdistilled.operp.client.account.models.impl.ReceivedTransactionModelImpl;
 
 @Configuration
 public class EntityModelContext {
@@ -19,5 +21,10 @@ public class EntityModelContext {
 	@Bean
 	public ReceivableAccountModel receivableAccountModel() {
 		return new ReceivableAccountModelImpl();
+	}
+
+	@Bean
+	public ReceivedTransactionModel receivedTransactionModel() {
+		return new ReceivedTransactionModelImpl();
 	}
 }
