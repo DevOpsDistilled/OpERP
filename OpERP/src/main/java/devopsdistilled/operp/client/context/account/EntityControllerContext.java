48d9a3f3db3579c37fcde62efe983b2af35bcefe
Kapil Koju <kplx2.0@gmail.com>
2013-08-25 01:19:56 +0545

    Bean Controller & Model for PaidTransaction

--- OpERP/src/main/java/devopsdistilled/operp/client/context/account/EntityControllerContext.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/context/account/EntityControllerContext.java
@@ -3,7 +3,9 @@ package devopsdistilled.operp.client.context.account;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import devopsdistilled.operp.client.account.controllers.PaidTransactionController;
 import devopsdistilled.operp.client.account.controllers.ReceivedTransactionController;
+import devopsdistilled.operp.client.account.controllers.impl.PaidTransactionControllerImpl;
 import devopsdistilled.operp.client.account.controllers.impl.ReceivedTransactionControllerImpl;
 
 @Configuration
@@ -12,5 +14,10 @@ public class EntityControllerContext {
 	@Bean
 	public ReceivedTransactionController receivedTransactionController() {
 		return new ReceivedTransactionControllerImpl();
+	}
+	
+	@Bean
+	public PaidTransactionController paidTransactionController() {
+		return new PaidTransactionControllerImpl();
 	}
 }
--- OpERP/src/main/java/devopsdistilled/operp/client/context/account/EntityModelContext.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/context/account/EntityModelContext.java
@@ -3,9 +3,11 @@ package devopsdistilled.operp.client.context.account;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import devopsdistilled.operp.client.account.models.PaidTransactionModel;
 import devopsdistilled.operp.client.account.models.PayableAccountModel;
 import devopsdistilled.operp.client.account.models.ReceivableAccountModel;
 import devopsdistilled.operp.client.account.models.ReceivedTransactionModel;
+import devopsdistilled.operp.client.account.models.impl.PaidTransactionModelImpl;
 import devopsdistilled.operp.client.account.models.impl.PayableAccountModelImpl;
 import devopsdistilled.operp.client.account.models.impl.ReceivableAccountModelImpl;
 import devopsdistilled.operp.client.account.models.impl.ReceivedTransactionModelImpl;
@@ -26,5 +28,10 @@ public class EntityModelContext {
 	@Bean
 	public ReceivedTransactionModel receivedTransactionModel() {
 		return new ReceivedTransactionModelImpl();
+	}
+	
+	@Bean
+	public PaidTransactionModel paidTransactionModel() {
+		return new PaidTransactionModelImpl();
 	}
 }
