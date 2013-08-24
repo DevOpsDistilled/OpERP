8fe2d501d29e427e0a7e32ae25661279c286bdef
Kapil Koju <kplx2.0@gmail.com>
2013-08-24 14:40:45 +0545

    Bean Account EntityModel

--- OpERP/src/main/java/devopsdistilled/operp/client/context/account/EntityModelContext.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/context/account/EntityModelContext.java
@@ -1,8 +1,23 @@
 package devopsdistilled.operp.client.context.account;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import devopsdistilled.operp.client.account.models.PayableAccountModel;
+import devopsdistilled.operp.client.account.models.ReceivableAccountModel;
+import devopsdistilled.operp.client.account.models.impl.PayableAccountModelImpl;
+import devopsdistilled.operp.client.account.models.impl.ReceivableAccountModelImpl;
 
 @Configuration
 public class EntityModelContext {
 
+	@Bean
+	public PayableAccountModel payableAccountModel() {
+		return new PayableAccountModelImpl();
+	}
+
+	@Bean
+	public ReceivableAccountModel receivableAccountModel() {
+		return new ReceivableAccountModelImpl();
+	}
 }
