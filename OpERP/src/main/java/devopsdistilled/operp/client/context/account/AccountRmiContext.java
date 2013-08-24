668dcd870a95fbb6191f24e2d84b7db1b8013c1e
Kapil Koju <kplx2.0@gmail.com>
2013-08-24 14:21:58 +0545

    Proxy Bean Transaction Services
    OPEN - task 87: Create Account module 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/87

--- OpERP/src/main/java/devopsdistilled/operp/client/context/account/AccountRmiContext.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/context/account/AccountRmiContext.java
@@ -5,8 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.remoting.rmi.RmiProxyFactoryBean;
 
 import devopsdistilled.operp.client.context.RmiContext;
+import devopsdistilled.operp.server.data.service.account.PaidTransactionService;
 import devopsdistilled.operp.server.data.service.account.PayableAccountService;
 import devopsdistilled.operp.server.data.service.account.ReceivableAccountService;
+import devopsdistilled.operp.server.data.service.account.ReceivedTransactionService;
 
 @Configuration
 public class AccountRmiContext extends RmiContext {
@@ -24,6 +26,24 @@ public class AccountRmiContext extends RmiContext {
 	public RmiProxyFactoryBean receivableAccountService() {
 		RmiProxyFactoryBean rmiProxy = new RmiProxyFactoryBean();
 		rmiProxy.setServiceInterface(ReceivableAccountService.class);
+		String serviceName = rmiProxy.getServiceInterface().getCanonicalName();
+		rmiProxy.setServiceUrl(rmiUrl + "/" + serviceName);
+		return rmiProxy;
+	}
+
+	@Bean
+	public RmiProxyFactoryBean paidTransactionService() {
+		RmiProxyFactoryBean rmiProxy = new RmiProxyFactoryBean();
+		rmiProxy.setServiceInterface(PaidTransactionService.class);
+		String serviceName = rmiProxy.getServiceInterface().getCanonicalName();
+		rmiProxy.setServiceUrl(rmiUrl + "/" + serviceName);
+		return rmiProxy;
+	}
+
+	@Bean
+	public RmiProxyFactoryBean receivedTransactionService() {
+		RmiProxyFactoryBean rmiProxy = new RmiProxyFactoryBean();
+		rmiProxy.setServiceInterface(ReceivedTransactionService.class);
 		String serviceName = rmiProxy.getServiceInterface().getCanonicalName();
 		rmiProxy.setServiceUrl(rmiUrl + "/" + serviceName);
 		return rmiProxy;
