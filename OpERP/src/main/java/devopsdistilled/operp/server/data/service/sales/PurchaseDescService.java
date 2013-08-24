c13f51ce344f38976c67aee3a6fe3eb7d76f4537
Kapil Koju <kplx2.0@gmail.com>
2013-08-25 02:47:33 +0545

    Add PurchaseDesc service

---
+++ OpERP/src/main/java/devopsdistilled/operp/server/data/service/sales/PurchaseDescService.java
@@ -0,0 +1,7 @@
+package devopsdistilled.operp.server.data.service.sales;
+
+import devopsdistilled.operp.server.data.entity.business.PurchaseDesc;
+
+public interface PurchaseDescService extends BusinessDescService<PurchaseDesc> {
+
+}
