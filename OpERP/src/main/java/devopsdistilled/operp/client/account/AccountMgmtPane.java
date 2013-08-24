61e130df213b20a76613d892325b5729dca3a041
Kapil Koju <kplx2.0@gmail.com>
2013-08-24 11:20:49 +0545

    Add Account Management Pane
    OPEN - task 87: Create Account module 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/87

---
+++ OpERP/src/main/java/devopsdistilled/operp/client/account/AccountMgmtPane.java
@@ -0,0 +1,44 @@
+package devopsdistilled.operp.client.account;
+
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import net.miginfocom.swing.MigLayout;
+import devopsdistilled.operp.client.abstracts.TaskPane;
+
+public class AccountMgmtPane extends TaskPane {
+
+	@Override
+	public String toString() {
+		return new String("Account");
+	}
+
+	/**
+	 * @wbp.parser.entryPoint
+	 */
+	@Override
+	public JComponent getPane() {
+		JPanel pane = new JPanel();
+		pane.setLayout(new MigLayout("", "[136px,grow]", "[15px][][]"));
+		pane.add(new JLabel("Account Management"),
+				"cell 0 0 2097051 1,alignx center,aligny top");
+
+		JButton btnReceivePayment = new JButton("Receive Payment");
+		btnReceivePayment.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+			}
+		});
+		btnReceivePayment.setFont(new Font("DejaVu Sans", Font.PLAIN, 12));
+		pane.add(btnReceivePayment, "cell 0 2");
+		return pane;
+	}
+
+}
