aa11f773bdac26e4a39358f128e964aef222a9ee
Kapil Koju <kplx2.0@gmail.com>
2013-08-24 14:37:30 +0545

    Bean MVC compo for ReceivedTransactionPane
    OPEN - task 87: Create Account module 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/87

--- OpERP/src/main/java/devopsdistilled/operp/client/context/account/MvcModelContext.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/context/account/MvcModelContext.java
@@ -1,8 +1,17 @@
 package devopsdistilled.operp.client.context.account;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import devopsdistilled.operp.client.account.panes.models.ReceivedTransactionPaneModel;
+import devopsdistilled.operp.client.account.panes.models.impl.ReceivedTransactionPaneModelImpl;
 
 @Configuration
 public class MvcModelContext {
+
+	@Bean
+	public ReceivedTransactionPaneModel receivedTransactionPaneModel() {
+		return new ReceivedTransactionPaneModelImpl();
+	}
 
 }
