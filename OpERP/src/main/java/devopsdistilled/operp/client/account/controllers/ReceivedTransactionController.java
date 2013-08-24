40963d79c6e6876a6f9613d12789cd4cbbdb741d
Kapil Koju <kplx2.0@gmail.com>
2013-08-24 14:26:08 +0545

    Add EntityController for ReceivedTransaction
    OPEN - task 87: Create Account module 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/87

---
+++ OpERP/src/main/java/devopsdistilled/operp/client/account/controllers/ReceivedTransactionController.java
@@ -0,0 +1,9 @@
+package devopsdistilled.operp.client.account.controllers;
+
+import devopsdistilled.operp.client.abstracts.EntityController;
+import devopsdistilled.operp.server.data.entity.account.ReceivedTransaction;
+
+public interface ReceivedTransactionController extends
+		EntityController<ReceivedTransaction> {
+
+}
---
+++ OpERP/src/main/java/devopsdistilled/operp/client/account/controllers/impl/ReceivedTransactionControllerImpl.java
@@ -0,0 +1,39 @@
+package devopsdistilled.operp.client.account.controllers.impl;
+
+import devopsdistilled.operp.client.account.controllers.ReceivedTransactionController;
+import devopsdistilled.operp.server.data.entity.account.ReceivedTransaction;
+
+public class ReceivedTransactionControllerImpl implements
+		ReceivedTransactionController {
+
+	@Override
+	public void create() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void edit(ReceivedTransaction receivedTransaction) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void list() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void delete(ReceivedTransaction receivedTransaction) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void showDetails(ReceivedTransaction receivedTransaction) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
