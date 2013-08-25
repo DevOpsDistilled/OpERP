ea2928381140081f6fa13bb6cb1d8b600a2d499e
Kapil Koju <kplx2.0@gmail.com>
2013-08-25 19:03:13 +0545

    Annotate with Service
    OPEN - task 88: Create HR module 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/88

--- OpERP/src/main/java/devopsdistilled/operp/server/data/service/hr/impl/EmployeeServiceImpl.java
+++ OpERP/src/main/java/devopsdistilled/operp/server/data/service/hr/impl/EmployeeServiceImpl.java
@@ -2,11 +2,14 @@ package devopsdistilled.operp.server.data.service.hr.impl;
 
 import javax.inject.Inject;
 
+import org.springframework.stereotype.Service;
+
 import devopsdistilled.operp.server.data.entity.hr.Employee;
 import devopsdistilled.operp.server.data.repo.hr.EmployeeRepository;
 import devopsdistilled.operp.server.data.service.hr.EmployeeService;
 import devopsdistilled.operp.server.data.service.impl.AbstractEntityService;
 
+@Service
 public class EmployeeServiceImpl extends
 		AbstractEntityService<Employee, Long, EmployeeRepository> implements
 		EmployeeService {
