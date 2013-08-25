363dbab99d8f928ac5c9197777b848946dd04e25
Kapil Koju <kplx2.0@gmail.com>
2013-08-25 18:59:33 +0545

    Add services for Employee
    OPEN - task 88: Create HR module 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/88

---
+++ OpERP/src/main/java/devopsdistilled/operp/server/data/service/hr/EmployeeService.java
@@ -0,0 +1,8 @@
+package devopsdistilled.operp.server.data.service.hr;
+
+import devopsdistilled.operp.server.data.entity.hr.Employee;
+import devopsdistilled.operp.server.data.service.EntityService;
+
+public interface EmployeeService extends EntityService<Employee, Long> {
+
+}
---
+++ OpERP/src/main/java/devopsdistilled/operp/server/data/service/hr/impl/EmployeeServiceImpl.java
@@ -0,0 +1,29 @@
+package devopsdistilled.operp.server.data.service.hr.impl;
+
+import javax.inject.Inject;
+
+import devopsdistilled.operp.server.data.entity.hr.Employee;
+import devopsdistilled.operp.server.data.repo.hr.EmployeeRepository;
+import devopsdistilled.operp.server.data.service.hr.EmployeeService;
+import devopsdistilled.operp.server.data.service.impl.AbstractEntityService;
+
+public class EmployeeServiceImpl extends
+		AbstractEntityService<Employee, Long, EmployeeRepository> implements
+		EmployeeService {
+
+	private static final long serialVersionUID = 2648525777561781085L;
+
+	@Inject
+	private EmployeeRepository repo;
+
+	@Override
+	protected EmployeeRepository getRepo() {
+		return repo;
+	}
+
+	@Override
+	protected Employee findByEntityName(String entityName) {
+		return null;
+	}
+
+}
