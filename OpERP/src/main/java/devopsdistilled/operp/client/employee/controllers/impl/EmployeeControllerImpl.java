dd4c97c96b7b75832c6c4e6bdc97226e93537bed
Kapil Koju <kplx2.0@gmail.com>
2013-08-26 00:37:59 +0545

    Impl create()
    OPEN - task 88: Create HR module 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/88

--- OpERP/src/main/java/devopsdistilled/operp/client/employee/controllers/impl/EmployeeControllerImpl.java
+++ OpERP/src/main/java/devopsdistilled/operp/client/employee/controllers/impl/EmployeeControllerImpl.java
@@ -2,19 +2,23 @@ package devopsdistilled.operp.client.employee.controllers.impl;
 
 import javax.inject.Inject;
 
+import devopsdistilled.operp.client.abstracts.EntityOperation;
 import devopsdistilled.operp.client.employee.controllers.EmployeeController;
 import devopsdistilled.operp.client.employee.models.EmployeeModel;
+import devopsdistilled.operp.client.employee.panes.controllers.EmployeePaneController;
 import devopsdistilled.operp.server.data.entity.employee.Employee;
 
 public class EmployeeControllerImpl implements EmployeeController {
+
+	@Inject
+	private EmployeePaneController employeePaneController;
 
 	@Inject
 	private EmployeeModel employeeModel;
 
 	@Override
 	public void create() {
-		// TODO Auto-generated method stub
-
+		employeePaneController.init(new Employee(), EntityOperation.Create);
 	}
 
 	@Override
