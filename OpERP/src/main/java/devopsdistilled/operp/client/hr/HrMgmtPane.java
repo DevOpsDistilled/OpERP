7ad064218fd36063de586427d267e728f72cf1b7
Kapil Koju <kplx2.0@gmail.com>
2013-08-25 19:11:26 +0545

    Basic UI for Employee Mgmt
    OPEN - task 88: Create HR module 
    http://github.com/DevOpsDistilled/OpERP/issues/issue/88

---
+++ OpERP/src/main/java/devopsdistilled/operp/client/hr/HrMgmtPane.java
@@ -0,0 +1,41 @@
+package devopsdistilled.operp.client.hr;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import net.miginfocom.swing.MigLayout;
+import devopsdistilled.operp.client.abstracts.TaskPane;
+
+public class HrMgmtPane extends TaskPane {
+
+	@Override
+	public String toString() {
+		return new String("Employees");
+	}
+
+	/**
+	 * @wbp.parser.entryPoint
+	 */
+	@Override
+	public JComponent getPane() {
+		JPanel pane = new JPanel();
+		pane.setLayout(new MigLayout("", "[136px,grow]", "[15px][][]"));
+		pane.add(new JLabel("Employee Management"),
+				"cell 0 0,alignx center,aligny top");
+
+		JButton btnRegisterNewEmployee = new JButton("Register New Employee");
+		btnRegisterNewEmployee.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		pane.add(btnRegisterNewEmployee, "cell 0 2");
+		return pane;
+	}
+
+}
