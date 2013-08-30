19e3a50ac4b01b1dc824fd5c6642412c68b642b3
Kapil Koju <kplx2.0@gmail.com>
2013-08-30 15:37:42 +0545

    Change argument to "init" for creating schema

--- OpERP/src/main/java/devopsdistilled/operp/server/ServerApp.java
+++ OpERP/src/main/java/devopsdistilled/operp/server/ServerApp.java
@@ -9,7 +9,7 @@ public class ServerApp {
 
 	public static void main(String[] args) {
 
-		if (args.length > 0 && args[0].equalsIgnoreCase("create"))
+		if (args.length > 0 && args[0].equalsIgnoreCase("init"))
 			System.setProperty("hibernate.hbm2ddl.auto", "create");
 		else
 			System.setProperty("hibernate.hbm2ddl.auto", "update");
