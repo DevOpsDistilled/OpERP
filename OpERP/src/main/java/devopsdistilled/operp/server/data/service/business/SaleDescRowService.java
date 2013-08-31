c0fc92663978c1fa85af310e155a958a2d277bd0
Kapil Koju <kplx2.0@gmail.com>
2013-08-31 19:03:56 +0545

    Remove DescRow services

--- OpERP/src/main/java/devopsdistilled/operp/server/data/service/business/SaleDescRowService.java
+++
@@ -1,7 +0,0 @@
-package devopsdistilled.operp.server.data.service.business;
-
-import devopsdistilled.operp.server.data.entity.business.SaleDescRow;
-
-public interface SaleDescRowService extends BusinessDescRowService<SaleDescRow> {
-
-}
